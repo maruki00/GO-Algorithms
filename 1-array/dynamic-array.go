b255c1a10f0d363b6ce0214404c6c288aba51481
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-04-10 20:58:23 +0300

    array

--- 1-array/dynamic-array.go
+++ 1-array/dynamic-array.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var items []int
+	items = append(items, 10)
+	items = append(items, 15)
+	items = append(items, 20)
+	items = append(items, 25)
+	fmt.Println("Items: ", items)
 }
