00a58443cf264cedc40904c1fad0d78d285d23aa
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-04-10 21:11:30 +0300

    stack

--- 2-stack/stack.go
+++ 2-stack/stack.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+type Stack struct {
+	items  []int
+	lenght int
+}
+
+func (obj *Stack) push(item int) {
+	obj.items = append([]int{item}, obj.items...)
+	obj.lenght++
+}
+
+func (obj *Stack) pop() int {
+	item := obj.items[0]
+	obj.items = obj.items[1:]
+	obj.lenght--
+	return item
+}
+
+func main() {
+	stack := &Stack{}
+	stack.push(1)
+	stack.push(12)
+	stack.push(123)
+	stack.push(1234)
+	p := stack.pop()
+	fmt.Println("items: ", stack.items, p)
 }
