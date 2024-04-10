4d67dd632567066bbdab10fa77cb82a767d51fb8
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-04-10 19:16:06 +0300

    array

---
+++ 1-array/stack.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	next *Node
+	item any
+}
+type Stack struct {
+	root *Node
+}
+
+func (obj *Stack) add(item any) {
+	newNode := &Node{next: nil, item: item}
+	if obj.root == nil {
+		obj.root = newNode
+	} else {
+		root := obj.root
+		for root.next != nil {
+			root = root.next
+		}
+		root.next = newNode
+	}
+}
+func main() {
+	stack := &Stack{}
+	stack.add(1234)
+	stack.add(12344536)
+	//stack.root = &Node{next: nil, item: 123}
+	fmt.Println("Stack: ", stack.root.item)
+}
