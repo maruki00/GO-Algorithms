bc65d49b7e2c6ced444e0fb80497eed55788b983
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-04-18 00:08:35 +0300

    graph

--- 6-Graph/graph.go
+++ 6-Graph/graph.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Graph struct {
 	Verteces []*Vertex
 }
+
 type Vertex struct {
 	key       int
 	adjancent []*Vertex
 }
 
 func (g *Graph) AddVertex(k int) {
+	if g.contains(g.Verteces, k) {
+		fmt.Println(fmt.Errorf("Vertex Alread Exists").Error())
+		return
+	}
 	g.Verteces = append(g.Verteces, &Vertex{key: k})
 }
 
@@ -21,6 +28,15 @@ func (g *Graph) Print() {
 			fmt.Printf(" %v, ", vk.key)
 		}
 	}
+}
+
+func (g *Graph) contains(s []*Vertex, k int) bool {
+	for _, v := range s {
+		if k == v.key {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
