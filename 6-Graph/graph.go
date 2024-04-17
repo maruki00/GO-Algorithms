f6fa9715d72ed88fe0bf1686ff8865a30cc4fe6b
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-04-17 23:54:10 +0300

    graph

--- 6-Graph/graph.go
+++ 6-Graph/graph.go
@@ -1,9 +1,24 @@
 package main
 
+import "fmt"
+
 type Graph struct {
 	Verteces []*Vertex
 }
 type Vertex struct {
 	key       int
 	adjancent []*Vertex
+}
+
+func (g *Graph) AddVertex(k int) {
+	g.Verteces = append(g.Verteces, &Vertex{key: k})
+}
+
+func (g *Graph) Print() {
+	for _, v := range g.Verteces {
+		fmt.Printf("\n Vertex %v : ", v.key)
+		for _, vk := range v.adjancent {
+			fmt.Printf(" %v, ", vk.key)
+		}
+	}
 }
