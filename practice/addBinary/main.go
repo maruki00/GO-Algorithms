2adfce96adf51599412b91c63e37886e3bea9ef3
4m4z1gh <aitbaha48@gmail.com>
2024-06-21 00:42:16 +0300

    --

--- practice/addBinary/main.go
+++ practice/addBinary/main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"math/big"
 )
 
 func addBinary(a string, b string) string {
-	num1, _ := strconv.ParseInt(a, 2, 1024)
-	num2, _ := strconv.ParseInt(b, 2, 1024)
-
-	return strconv.FormatInt(num1+num2, 2)
+	i := big.NewInt(0)
+	j := big.NewInt(0)
+	i.SetString(a, 2)
+	j.SetString(b, 2)
+	res := i.Add(i, j)
+	return fmt.Sprintf("%b", res)
 }
 
 func main() {
 
-	fmt.Println(addBinary("11", "1"))
+	fmt.Println(addBinary("000010100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+		"110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"))
 }
