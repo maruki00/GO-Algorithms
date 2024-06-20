47b2a0e732773fd5152c5399af3354ebba543a5c
4m4z1gh <aitbaha48@gmail.com>
2024-06-20 23:45:17 +0300

    --

--- practice/addBinary/main.go
+++ practice/addBinary/main.go
@@ -6,14 +6,13 @@ import (
 )
 
 func addBinary(a string, b string) string {
-	num1, _ := strconv.ParseInt(a, 2, 32)
-	num2, _ := strconv.ParseInt(b, 2, 32)
+	num1, _ := strconv.ParseInt(a, 2, 1024)
+	num2, _ := strconv.ParseInt(b, 2, 1024)
 
-	println(num1, num2)
-	return "111"
+	return strconv.FormatInt(num1+num2, 2)
 }
 
 func main() {
 
-	fmt.Println(addBinary("11", "11"))
+	fmt.Println(addBinary("11", "1"))
 }
