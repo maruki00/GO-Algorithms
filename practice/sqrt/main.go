f5966b476cda654b1c287d90ea37f95c03787be4
4m4z1gh <aitbaha48@gmail.com>
2024-06-21 02:53:27 +0300

    --

--- practice/sqrt/main.go
+++ practice/sqrt/main.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
-func mySqrt(x int) int {
-	sqrt := x / 2
-	tmp := 0
-
-	for tmp != sqrt {
-		tmp = sqrt
-		sqrt = (x/tmp + tmp) / 2
+func mySqrt1(x int) int {
+	if x < 2 {
+		return x
 	}
-	return sqrt
+	y := float64(x)
+	var sqrt float64 = float64((y + (float64(x) / float64(y))) / 2)
+
+	for float64(y-sqrt) >= 0.00001 {
+		y = sqrt
+		sqrt = float64((y + (float64(x) / float64(y))) / 2)
+
+	}
+	return int(sqrt)
+}
+
+func mySqrt(x int) int {
+	l, r := 0, x
+	for l <= r {
+		mid := (l + r) / 2
+		if mid*mid == x {
+			return mid
+		} else if mid*mid > x {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return r
 }
 
 func main() {
