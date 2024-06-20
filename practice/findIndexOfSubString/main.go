428ecb008f1b8b9f3aba7bbaa23e0c7dc4b9a5cb
4m4z1gh <aitbaha48@gmail.com>
2024-06-20 21:55:59 +0300

    --

--- practice/findIndexOfSubString/main.go
+++ practice/findIndexOfSubString/main.go
@@ -6,6 +6,7 @@ func strStr(haystack string, needle string) int {
 
 	lenHaystacke := len(haystack)
 	lenNeedle := len(needle)
+
 	if haystack == needle {
 		return 0
 	}
@@ -15,15 +16,16 @@ func strStr(haystack string, needle string) int {
 	}
 
 	for i := 0; i < lenHaystacke; i++ {
-		if haystack[i] == needle[0] && haystack[i:i+lenNeedle] == needle {
+		if haystack[i] == needle[0] && haystack[i:i+lenNeedle+1] == needle {
 			return i
 		}
 	}
+
 	return -1
 }
 
 func main() {
-	string1 := "hello world"
-	needl := "llo"
-	fmt.Println("result : ", string1[2:4], string1[2:5] == needl)
+	haystack := "sadbutsad"
+	needle := "sad"
+	fmt.Println("result : ", strStr(haystack, needle))
 }
