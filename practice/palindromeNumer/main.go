9c61d1b483cca111acd75dd73c89037d02fe6c1f
4m4z1gh <aitbaha48@gmail.com>
2024-06-20 16:24:03 +0300

    --

--- practice/palindromeNumer/main.go
+++ practice/palindromeNumer/main.go
@@ -1,24 +1,34 @@
-package main 
+package main
 
+import "fmt"
 
-func numToArray(num int ) int[]{
-    var arr int[]
-    for num > 0 {
+func numToArray(num int) []int {
+	var arr []int
+	for num > 0 {
 		arr = append(arr, num%10)
-		num = (int)(num/10)
-    }
+		num = (int)(num / 10)
+	}
 	return arr
 }
 
 func isPalindrome(x int) bool {
-    if x <= 10{
-        return false
-    }
+	if x <= 10 {
+		return false
+	}
+	nums := numToArray(x)
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		if nums[left] != nums[right] {
+			return false
+		}
+	}
 
+	return true
 
 }
 
-func main(){
-
+func main() {
+	x := 123
+	fmt.Println(isPalindrome(x))
 }
-
