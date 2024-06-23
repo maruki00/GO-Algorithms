9651c712a694c1700a6d39479a95b3a61750f43c
4m4z1gh <aitbaha48@gmail.com>
2024-06-23 15:57:04 +0300

    optimization

--- practice/medianofTwoSortedArrays/main.go
+++ practice/medianofTwoSortedArrays/main.go
@@ -1,18 +1,36 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
+
+func _findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	nums1 = append(nums1, nums2...)
+	sort.Ints(nums1)
+	lenNums := len(nums1)
+	if lenNums%2 != 0 {
+		return float64(nums1[int(lenNums/2)])
+	} else {
+		val := (nums1[int(lenNums/2)] + nums1[int(lenNums/2)+1]) / 2
+		println(val)
+		return float64(val)
+	}
+}
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	nums1 = append(nums1, nums2...)
 	sort.Ints(nums1)
 	lenNums := len(nums1)
 	if lenNums%2 != 0 {
-		return float64(lenNums)
+		return float64(nums1[int(lenNums/2)])
 	} else {
-		return float64((lenNums / 2) + ((lenNums/2)+1)/2)
+		return float64((float64(nums1[int(lenNums/2)]) + float64(nums1[int(lenNums/2)-1])) / 2)
 	}
 }
 
 func main() {
-
+	nums1 := []int{1, 2}
+	nums2 := []int{3, 4}
+	fmt.Println("val : ", findMedianSortedArrays(nums1, nums2))
 }
