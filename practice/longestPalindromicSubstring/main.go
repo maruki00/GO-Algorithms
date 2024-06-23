7af0f61282dcd1a9ef5e5b42b62fe70d65123997
4m4z1gh <aitbaha48@gmail.com>
2024-06-23 14:34:45 +0300

    --

--- practice/longestPalindromicSubstring/main.go
+++ practice/longestPalindromicSubstring/main.go
@@ -1,10 +1,38 @@
 package main
 
+import "fmt"
+
+func reverseStr(s string) (result string) {
+	for _, ss := range s {
+		result = string(ss) + result
+	}
+	return result
+}
+
 func longestPalindrome(s string) string {
 
-	return ""
+	longest := ""
+	for i := 0; i < len(s); i++ {
+		left, right := i, len(s)-1
+		fmt.Println("Level 1 : ", i)
+		for left < right {
+			fmt.Println("Level 2 : ", left, right)
+			if s[left] == s[right] && (right-left) > len(longest) {
+				fmt.Println("Found ....", s[left:right+1], reverseStr(s[left:right+1]))
+				if s[left:right+1] == reverseStr(s[left:right+1]) {
+					longest = s[left : right+1]
+				}
+			}
+			right--
+		}
+		left++
+	}
+
+	return longest
 }
 
 func main() {
 
+	s := "dabad"
+	fmt.Println(longestPalindrome(s))
 }
