91916bc7ab45383c15fbb89b5248f6d453283884
4m4z1gh <aitbaha48@gmail.com>
2024-06-25 01:33:40 +0300

    optimization

--- practice/stringToInteger/main.go
+++ practice/stringToInteger/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"strings"
 	"unicode"
 )
@@ -10,7 +9,7 @@ import (
 const MAXINT = 2147483647
 const MININT = -2147483648
 
-func myAtoi_(s string) int {
+func myAtoi(s string) int {
 	s = strings.Trim(s, " ")
 	result := 0
 
@@ -26,7 +25,7 @@ func myAtoi_(s string) int {
 		s = s[1:]
 	}
 	for _, char := range s {
-		if char < '0' || char > '9' {
+		if !unicode.IsDigit(char) {
 			break
 		}
 
@@ -39,39 +38,7 @@ func myAtoi_(s string) int {
 		}
 
 	}
-	return (result) * sign
-}
-func myAtoi(s string) int {
-	sign := 1
-	res := 0
-	s = strings.TrimSpace(s)
-
-	if len(s) == 0 {
-		return res
-	}
-
-	if s[0] == '-' {
-		sign = -1
-		s = s[1:]
-	} else if s[0] == '+' {
-		s = s[1:]
-	}
-
-	for _, ch := range s {
-		if !unicode.IsDigit(ch) {
-			break
-		}
-		dig := int(ch - '0')
-		res = res*10 + dig
-
-		if sign*res < math.MinInt32 {
-			return math.MinInt32
-		} else if sign*res > math.MaxInt32 {
-			return math.MaxInt32
-		}
-	}
-
-	return sign * res
+	return result * sign
 }
 
 func main() {
