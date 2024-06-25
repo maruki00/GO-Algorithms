a9e27ccdc8b7f605a4783f1d25aa91b988efb4c3
4m4z1gh <aitbaha48@gmail.com>
2024-06-25 05:01:08 +0300

    optimization

--- practice/maximumDepthofBinaryTree/main.go
+++ practice/maximumDepthofBinaryTree/main.go
@@ -7,12 +7,28 @@ type TreeNode struct {
 }
 
 func maxDepth(root *TreeNode) int {
-	depth := 0
-	var godepth func(root *TreeNode)
-	godepth = func(root *TreeNode) {
-		depth = 10
+	if root == nil {
+		return 0
 	}
-	godepth(root)
+
+	depth := 0
+	var dfs func(root *TreeNode, dep int)
+	dfs = func(root *TreeNode, dep int) {
+		if root == nil {
+			return
+		}
+
+		dfs(root.Left, dep+1)
+		if dep > depth {
+			depth = dep
+		}
+		dfs(root.Right, dep+1)
+		if dep > depth {
+			depth = dep
+		}
+
+	}
+	dfs(root, 1)
 	return depth
 }
 func main() {
