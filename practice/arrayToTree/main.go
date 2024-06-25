40553a3dcd51d376e94227a2c0548ff2c72b9a6c
4m4z1gh <aitbaha48@gmail.com>
2024-06-26 01:34:13 +0300

    optimization

---
+++ practice/arrayToTree/main.go
@@ -0,0 +1,17 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sortedArrayToBTS(nums []int) *TreeNode {
+
+	return &TreeNode{0, nil, nil}
+
+}
+
+func main() {
+
+}
