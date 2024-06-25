e66a106b07b14d85ee1b1edf092a4fde45cb472b
4m4z1gh <aitbaha48@gmail.com>
2024-06-25 05:25:15 +0300

    optimization

--- practice/maximumDepthofBinaryTree/main.go
+++ practice/maximumDepthofBinaryTree/main.go
@@ -8,30 +8,30 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func maxDepth(root *TreeNode) int {
+func maxDepth_(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-
-	depth := 0
-	var dfs func(root *TreeNode, dep int)
-	dfs = func(root *TreeNode, dep int) {
+	var dfs func(root *TreeNode, dep int) int
+	dfs = func(root *TreeNode, dep int) int {
 		if root == nil {
-			return
+			return dep
 		}
-
-		dfs(root.Left, dep+1)
-		if dep > depth {
-			depth = dep
-		}
-		dfs(root.Right, dep+1)
-		if dep > depth {
-			depth = dep
-		}
-
+		l := dfs(root.Left, dep+1)
+		r := dfs(root.Right, dep+1)
+		return max(l, r)
 	}
-	dfs(root, 1)
-	return depth
+	return dfs(root, 0)
+}
+func maxDepth(root *TreeNode) int {
+	var dfs func(root *TreeNode, dep int) int
+	dfs = func(root *TreeNode, dep int) int {
+		if root == nil {
+			return dep
+		}
+		return max(dfs(root.Left, dep+1), dfs(root.Right, dep+1))
+	}
+	return dfs(root, 0)
 }
 func main() {
 	root := &TreeNode{3, &TreeNode{9, nil, nil}, &TreeNode{20, &TreeNode{15, nil, nil}, &TreeNode{7, nil, &TreeNode{5, nil, nil}}}}
