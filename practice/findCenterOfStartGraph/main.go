4af218c91e86771d48245962ede6ec2fdd098b88
4m4z1gh <aitbaha48@gmail.com>
2024-06-27 19:42:53 +0300

    --

--- practice/findCenterOfStartGraph/main.go
+++ practice/findCenterOfStartGraph/main.go
@@ -2,19 +2,36 @@ package main
 
 import "fmt"
 
+func max(set map[int]int, key int, value int) int {
+	if set[key] > value {
+		return key
+	}
+	return value
+}
 func findCenter(edges [][]int) int {
-	var hashSet map[int]int = map[int]int{}
-	hashSet[1] = 1
 
-	return 0
+	var hashSet map[int]int = map[int]int{}
+	centerEdge := -1
+	maxEdges := 0
+	for _, i := range edges {
+		for _, j := range i {
+			hashSet[j] += 1
+			if hashSet[j] > maxEdges {
+				maxEdges = hashSet[j]
+				centerEdge = j
+			}
+		}
+	}
+
+	return centerEdge
 }
 
 func main() {
 	nums := [][]int{
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
-		{1, 2, 3},
+		{1, 2},
+		{5, 1},
+		{1, 3},
+		{1, 4},
 	}
-	fmt.Println(nums)
+	fmt.Println(findCenter(nums))
 }
