8a4e63da5a8ed5b36c297b13fae704fceca69391
4m4z1gh <aitbaha48@gmail.com>
2024-06-27 19:19:58 +0300

    --

---
+++ practice/findCenterOfStartGraph/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func findCenter(edges [][]int) int {
+	var hashSet map[int]int = map[int]int{}
+	hashSet[1] = 1
+
+	return 0
+}
+
+func main() {
+	nums := [][]int{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	fmt.Println(nums)
+}
