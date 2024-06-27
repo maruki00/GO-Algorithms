afd233f9316e4b9468300adca24c7ee103bb374a
4m4z1gh <aitbaha48@gmail.com>
2024-06-27 21:41:13 +0300

    --

--- practice/evaluateReversePoslishNation/main.go
+++ practice/evaluateReversePoslishNation/main.go
@@ -1,30 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Stack struct {
-	items []float32
+	items []int
 }
 
-func (obj *Stack) pop() float32 {
-	l := len(obj.items) - 1
+func (obj *Stack) pop() int {
+	l := len(obj.items)
+	fmt.Println("Pop Index: ", l, obj.items)
 	item := obj.items[l]
 	obj.items = obj.items[:l]
 	return item
 }
 
-func (obj *Stack) ppush(val float32) {
+func (obj *Stack) push(val int) {
 	obj.items = append(obj.items, val)
 }
 
 func evalRPN(tokens []string) int {
 	s := &Stack{}
-	fmt.Println(s)
 
-	return 0
+	x := 0
+	y := 0
+
+	for _, token := range tokens {
+
+		switch token {
+		case "+":
+			x = s.pop()
+			y = s.pop()
+			s.push(x + y)
+			fmt.Println("+", y, x)
+		case "-":
+			x = s.pop()
+			y = s.pop()
+			s.push(x - y)
+			fmt.Println("-", y, x)
+		case "*":
+			x = s.pop()
+			y = s.pop()
+			s.push(x * y)
+			fmt.Println("*", y, x)
+		case "/":
+			x = s.pop()
+			y = s.pop()
+			s.push(x / y)
+			fmt.Println("/", y, x)
+		default:
+			if item, err := strconv.Atoi(token); err == nil {
+				s.push(item)
+			}
+			fmt.Println("default", y, x)
+		}
+		fmt.Println("token: ", token, s.items)
+	}
+
+	return s.pop()
 }
 
 func main() {
 	token := []string{"2", "1", "+", "3", "*"}
 	fmt.Println(evalRPN(token))
+
 }
