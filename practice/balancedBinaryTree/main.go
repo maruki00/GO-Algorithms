2df8ece557e4574499e39265d88cbed7c4bc61ee
4m4z1gh <aitbaha48@gmail.com>
2024-06-27 06:18:11 +0300

    --

--- practice/balancedBinaryTree/main.go
+++ practice/balancedBinaryTree/main.go
@@ -9,8 +9,20 @@ type TreeNode struct {
 }
 
 func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
 
-	return false
+	var depth func(root *TreeNode, start int) int
+	depth = func(node *TreeNode, start int) int {
+		if node == nil {
+			return 0
+		}
+		return max(depth(node.Left, start+1), depth(node.Right, start+1)) + 1
+	}
+
+	diff := depth(root.Left, 0) - depth(root.Right, 0)
+	return diff >= -1 && diff <= 1 && (isBalanced(root.Left) && isBalanced(root.Right))
 }
 
 func main() {
