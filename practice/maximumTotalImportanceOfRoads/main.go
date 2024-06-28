a875272b1b5ebcdf41ad03f83a4cdfe0fef73460
4m4z1gh <aitbaha48@gmail.com>
2024-06-28 22:25:59 +0300

    --

--- practice/maximumTotalImportanceOfRoads/main.go
+++ practice/maximumTotalImportanceOfRoads/main.go
@@ -5,22 +5,65 @@ import (
 	"sort"
 )
 
-func maximunImportance(n int, roads [][]int) int64 {
-	hashSet := make(map[int]int)
-	for _, i := range roads {
-		hashSet[i[0]]++
-		hashSet[i[1]]++
-	}
-	keys := []int{}
-	for _, i := range hashSet {
-		keys = append(keys, i)
-	}
-	sort.SliceStable(keys, func(i, j int) bool {
-		return hashSet[keys[i]] < hashSet[keys[j]]
-	})
+// func maximunImportance(n int, roads [][]int) int64 {
+// 	hashSet := make(map[int][]int, n)
+// 	result := int64(0)
 
-	fmt.Println("Result : ", hashSet)
-	return 0
+// 	for _, i := range roads {
+// 		if len(hashSet[i[0]]) == 0 {
+// 			hashSet[i[0]] = []int{0, 0}
+// 		}
+// 		if len(hashSet[i[1]]) == 0 {
+// 			hashSet[i[1]] = []int{0, 0}
+// 		}
+
+// 		hashSet[i[0]][0] = hashSet[i[0]][0] + 1
+// 		hashSet[i[1]][0] = hashSet[i[1]][0] + 1
+// 	}
+
+// 	keys := make([]int, 0, n)
+// 	for i := range hashSet {
+// 		keys = append(keys, i)
+// 	}
+// 	sort.SliceStable(keys, func(i, j int) bool {
+// 		if hashSet[keys[i]][0] == hashSet[keys[j]][0] {
+// 			return i < j
+// 		} else {
+// 			return hashSet[keys[i]][0] < hashSet[keys[j]][0]
+// 		}
+// 	})
+
+// 	for i,j := range keys {
+
+// 	}
+
+// 	fmt.Println(keys)
+
+// 	// res := make(map[int][]int, 5)
+// 	// for i := 0; i < len(keys); i++ {
+// 	// 	result += int64()
+// 	// 	res[keys[i]] = append(res[keys[i]], []int{hashSet[keys[i]], i + 1}...)
+// 	// }
+// 	// fmt.Println(hashSet, keys, res)
+// 	return result
+// }
+
+func maximumImportance(n int, roads [][]int) int64 {
+	items := make([]int, n)
+	for _, e := range roads {
+		items[e[0]]++
+		items[e[1]]++
+	}
+
+	fmt.Println(items)
+	sort.Ints(items)
+	fmt.Println(items)
+
+	var res int64 = 0
+	for i := 0; i < n; i++ {
+		res += int64(i+1) * int64(items[i])
+	}
+	return res
 }
 
 func main() {
@@ -32,5 +75,5 @@ func main() {
 		{1, 3},
 		{2, 4},
 	}
-	fmt.Println(maximunImportance(5, roads))
+	fmt.Println(maximumImportance(5, roads))
 }
