332025067aff4a7bdd0a6f72577eeb620c23755b
4m4z1gh <aitbaha48@gmail.com>
2024-06-30 04:41:45 +0300

    optimize

--- practice/contiansDuplicate2/main.go
+++ practice/contiansDuplicate2/main.go
@@ -1,17 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func containsNearbyDuplicate(nums []int, k int) bool {
 
 	hashSet := make(map[int][]int)
-	for _, i := range nums {
-		fmt.Println(hashSet[i][0])
+	for index, i := range nums {
+		if len(hashSet[i]) == 0 {
+			hashSet[i] = append(hashSet[i], index)
+			continue
+		}
+		found := false
+		for _, j := range hashSet[i] {
+			if int(math.Abs(math.Inf(index-j))) > k {
+				found = true
+			}
+		}
+		if found {
+			return true
+		}
 	}
 	return false
 }
 
 func main() {
 	nums := []int{1, 2, 3, 1, 2, 3}
-	_ = containsNearbyDuplicate(nums, 3)
+	fmt.Println(containsNearbyDuplicate(nums, 2))
 }
