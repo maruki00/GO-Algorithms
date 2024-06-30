2e7bc53b5e52903ff9c8a90aeaa870fd864d58cd
4m4z1gh <aitbaha48@gmail.com>
2024-07-01 02:38:03 +0300

    optimize

--- practice/treeSum/main.go
+++ practice/treeSum/main.go
@@ -1,46 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
+func isExists(nums [][]int, num [3]int) bool {
+	for _, x := range nums {
+		if num[0] == x[0] && num[1] == x[1] && num[2] == x[2] {
+			return true
+		}
+	}
+	return false
+}
 func threeSum(nums []int) [][]int {
 	l := len(nums) - 1
 	hashMap := make(map[int][]int)
 	var result [][]int = [][]int{}
 	for j, i := range nums {
-		hashMap[i] =  append(hashMap[i], j)
+		hashMap[i] = append(hashMap[i], j)
 	}
+	fmt.Println(hashMap)
 	for index, _ := range nums {
-		left, right := index+1, l
+		left := index + 1
 		for left < l {
-
-			/*if nums[index]+nums[left]+nums[right] == 0 {
-				result = append(result, []int{nums[index], nums[left], nums[right]})
-				break
-			}*/
 
 			lNum := nums[index] + nums[left]
 			if _, ok := hashMap[0-lNum]; ok {
-				found := false
-				for _,m := range hashMap[0-lNum]{
-					if m > left{
-						result = append(result, []int{nums[index], nums[left], 0 - lNum})
-						found = true
-						break
-				}
-				if found {
-					break
+				for _, m := range hashMap[0-lNum] {
+					if m > left {
+						tmp := [3]int{nums[index], nums[left], 0 - lNum}
+						sort.Ints(tmp)
+						if isExists(result, tmp) == false {
+							result = append(result, tmp[:])
+							break
+						}
+
+					}
 				}
 			}
 
-			/*rNum := nums[index] + nums[right]
-			if _, ok := hashMap[0-rNum]; ok {
-				result = append(result, []int{nums[index], nums[right], 0 - rNum})
-				break
-			}
-			right--
 			left++
-			*/
-			left ++
 		}
 
 	}
