cd0a290d79860451d224bdb027ff7cc5d2732f0d
4m4z1gh <aitbaha48@gmail.com>
2024-06-30 06:22:59 +0300

    optimize

--- practice/missingNumber/main.go
+++ practice/missingNumber/main.go
@@ -8,13 +8,16 @@ import (
 func missingNumber(nums []int) int {
 
 	sort.Ints(nums)
-	for i := 0; i < len(nums); i++ {
-		if nums[i] != nums[i]+1 {
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i+1] != nums[i]+1 {
 			return nums[i] + 1
 		}
 	}
+	if nums[0] != 1 {
+		return 0
+	}
 
-	return 0
+	return nums[len(nums)-1] + 1
 }
 
 func main() {
