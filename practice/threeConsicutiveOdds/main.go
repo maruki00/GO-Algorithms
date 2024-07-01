19415a5e4608686bd3ba44628060357fe849fcf7
4m4z1gh <aitbaha48@gmail.com>
2024-07-01 04:25:40 +0300

    optimize

--- practice/threeConsicutiveOdds/main.go
+++ practice/threeConsicutiveOdds/main.go
@@ -1,10 +1,19 @@
 package main
 
-func threeConsecutiveOdds(arr p[int]) bool {
+func threeConsecutiveOdds(arr []int) bool {
 	for i := 0; i < len(arr)-2; i++ {
-		if arr[i]%2 != 0 && arr[i+1]%2 != 0 && arr[i+2]%2 != 0 {
-			return true
+		if arr[i]%2 == 0 {
+			continue
 		}
+		if arr[i+1]%2 == 0 {
+			continue
+		}
+		if arr[i+2]%2 == 0 {
+			continue
+		}
+		//if arr[i]%2 != 0 && arr[i+1]%2 != 0 && arr[i+2]%2 != 0 {
+		return true
+		//}
 	}
 	return false
 }
