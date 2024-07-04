cea13b820d851a90527867c82b5ef61eb0bd4eb1
4m4z1gh <aitbaha48@gmail.com>
2024-07-04 05:57:59 +0300

    optimize

--- practice/validAnagraM/main.go
+++ practice/validAnagraM/main.go
@@ -3,8 +3,17 @@ package main
 import "fmt"
 
 func isAnagram(s string, t string) bool {
-	for i, j := range s {
-		fmt.Println("result : ", i, j)
+	hashMap := make(map[rune]bool)
+	for _, j := range s {
+		hashMap[j] = true
+	}
+	for _, j := range t {
+		hashMap[j] = false
+	}
+	for _, j := range hashMap {
+		if j == true {
+			return false
+		}
 	}
 	return true
 }
