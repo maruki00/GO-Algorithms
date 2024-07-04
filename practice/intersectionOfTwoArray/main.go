ec33b36bf44442eb3fbd45f841da3aa38a68de18
4m4z1gh <aitbaha48@gmail.com>
2024-07-04 04:52:35 +0300

    optimize

--- practice/intersectionOfTwoArray/main.go
+++ practice/intersectionOfTwoArray/main.go
@@ -31,7 +31,7 @@ func intersection(nums1 []int, nums2 []int) []int {
 }
 
 func main() {
-	nums1 := []int{1, 2, 2, 1}
-	nums2 := []int{2, 2}
+	nums1 := []int{4, 9, 5}
+	nums2 := []int{9, 4, 9, 8, 4}
 	fmt.Println("Result : ", intersection(nums1, nums2))
 }
