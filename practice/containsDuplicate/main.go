8e4d9ad99a1bf46e0c91ea95058248a35b9c1e72
4m4z1gh <aitbaha48@gmail.com>
2024-07-08 00:06:14 +0300

    optimize

--- practice/containsDuplicate/main.go
+++ practice/containsDuplicate/main.go
@@ -5,7 +5,7 @@ import (
 	"sort"
 )
 
-func containsDuplicate(nums []int) bool {
+func _containsDuplicate(nums []int) bool {
 
 	sort.Ints(nums)
 	for i := 0; i < len(nums)-1; i++ {
@@ -16,7 +16,18 @@ func containsDuplicate(nums []int) bool {
 	return false
 }
 
-func main() {
+func containsDuplicate(nums []int) bool {
+	hashMap := make(map[int]int)
+	for i,j := range nums {
+		hashMap[j]++
+		if hashMap[j]>1{
+			return true
+		}
+	}
+	retunr false
+}
+
+func main() {:
 	nums := []int{1, 2, 3, 1}
 	fmt.Println("Result : ", containsDuplicate(nums))
 }
