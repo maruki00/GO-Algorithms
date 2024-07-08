684d85dae4660fe0aab0bfa7569e71cd0d6e7d77
4m4z1gh <aitbaha48@gmail.com>
2024-07-08 05:06:28 +0300

    optimize

--- practice/groupAnagrams/main.go
+++ practice/groupAnagrams/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strings"
 )
 
 func groupAnagrams(strs []string) [][]string {
-
-	hashMap := make(map[string][]string)
+	// hashMap := make(map[string][]string)
+	hashMap := make(map[[26]int][]string)
 	result := make([][]string, 0)
 	for _, j := range strs {
-		tmp := strings.Split(j, "")
-		sort.Strings(tmp)
-		key := strings.Join(tmp, "")
-		hashMap[key] = append(hashMap[key], j)
+		// tmp := strings.Split(j, "")
+		// sort.Strings(tmp)
+		// key := strings.Join(tmp, "")
+		// hashMap[key] = append(hashMap[key], j)
+
+		tmp := [26]int{}
+		for _, j := range j {
+			tmp[int(j)-97] += 1
+		}
+		hashMap[tmp] = append(hashMap[tmp], j)
+
 	}
 	for _, i := range hashMap {
 		result = append(result, i)
