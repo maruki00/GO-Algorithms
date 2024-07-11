0cc3a3f999db9ace5546dfbfed62b2140cc6e970
4m4z1gh <aitbaha48@gmail.com>
2024-07-11 21:41:13 +0300

    --

---
+++ practice/removeStartsFromString/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func removeStarts(s string) string {
+	stack := []string{}
+	for _, i := range s {
+		if string(i) == "*" {
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+
+		} else {
+			stack = append(stack, string(i))
+		}
+	}
+
+	return strings.Join(stack, "")
+
+}
+
+func main() {
+	fmt.Println("result : ", removeStarts("stack**leet*code"))
+}
