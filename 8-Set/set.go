78af5180b04bda1726d4e5c8f7b45a39ba1983f9
4m4z1gh <aitbaha48@gmail.com>
2024-07-15 14:33:49 +0300

    --

---
+++ 8-Set/set.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type Set struct {
+	elements map[any]bool
+}
+
+func NewSet(items ...any) *Set {
+	s := &Set{}
+	s.elements = make(map[any]bool)
+	for _, item := range items {
+		s.elements[item] = true
+	}
+	return s
+}
+func (s *Set) Add(items ...any) {
+	for _, item := range items {
+		s.elements[item] = true
+	}
+}
+
+func (s *Set) Delete(item any) {
+	delete(s.elements, item)
+}
+func (s Set) Get() []any {
+	items := []any{}
+	for i, _ := range s.elements {
+		items = append(items, i)
+	}
+	return items
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
