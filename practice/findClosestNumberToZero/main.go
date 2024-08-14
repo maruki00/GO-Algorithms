329b8a43af2d8389da146266e6d14625926778af
maruki00 <aitbaha48@gmail.com>
2024-08-14 10:40:57 +0300

    --

--- practice/findClosestNumberToZero/main.go
+++ practice/findClosestNumberToZero/main.go
@@ -2,21 +2,41 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
+func abs(a int) int {
+	if a < 0 {
+		return a * -1
+	}
+	return a
+}
+
 func findClosestNumber(nums []int) int {
-	closest := int(math.Abs(float64(nums[0])))
+	closest := abs(nums[0])
+	index := 0
 	for i := 1; i < len(nums); i++ {
-		if 0-int(math.Abs(float64(nums[i]))) >= 0-int(math.Abs(float64(closest))) {
-			closest = nums[i]
+		if abs(nums[index]) == abs(nums[i]) && nums[index] > nums[i] {
+			continue
+		}
+
+		if abs(0-abs(nums[i])) <= closest {
+
+			index = i
+			closest = abs(nums[i])
 		}
 	}
-	return closest
+	return nums[index]
 }
 
 func main() {
 	// nums := []int{-4, -2, 1, 4, 8}
-	nums := []int{2, -1, 1}
-	fmt.Println("result : ", findClosestNumber(nums))
+	nums := [][]int{
+		{2, -1, 1},
+		{-4, -2, 1, 4, 8},
+		{2, -1, 1},
+	}
+
+	for _, num := range nums {
+		fmt.Println("result : ", findClosestNumber(num))
+	}
 }
