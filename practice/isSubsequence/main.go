0dca518c71266b1f4906a68bd31cbcfb97114ed4
maruki00 <aitbaha48@gmail.com>
2024-08-14 11:28:40 +0300

    --

--- practice/isSubsequence/main.go
+++ practice/isSubsequence/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func isSubsequence(s string, t string) bool {
+	j := 0
+	for i := 0; i < len(t); i++ {
+		if s[j] == t[i] {
+			j++
+		}
+	}
+	fmt.Println("i-j : ", j)
+	return j == len(s)
+}
+
+func main() {
+	// s := "gdc"
+	// t := "ahbgdc"
+
+	s := "abc"
+	t := "ahbgdc"
+
+	fmt.Println("result : ", isSubsequence(s, t))
 }
