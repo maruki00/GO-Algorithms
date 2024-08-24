b4947d87747fb266243da0c0761d2f99a134322d
maruki00 <aitbaha48@gmail.com>
2024-08-25 02:24:12 +0300

    --

--- practice/spiralMatrix/main.go
+++ practice/spiralMatrix/main.go
@@ -1,17 +1,26 @@
 package main
 
+import "fmt"
+
 func spiralOrder(matrix [][]int) []int {
 
-	for i:=0; i<len(matrix[0]); i++ {
-		if i<len(matrix){
-			for j:= 0; j<len(matrix[0]) j++{
-				fmt.Print(matrix[i][j])
-			} 
+	ret := []int{}
+	for i := 0; i < len(matrix[0]); i++ {
+		if i < len(matrix) {
+			for j := 0; j < len(matrix[0]); j++ {
+				fmt.Print(matrix[i][j], " - ")
+			}
+			break
 		}
-        
-    }
+	}
+	return ret
 }
 
-func main(){
-
+func main() {
+	matrix := [][]int{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+	}
+	fmt.Println("result : ", spiralOrder(matrix))
 }
