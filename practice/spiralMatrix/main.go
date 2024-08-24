99fc319e59f59f20c46b48ecef8b8586e40c9410
maruki00 <aitbaha48@gmail.com>
2024-08-25 02:07:45 +0300

    --

---
+++ practice/spiralMatrix/main.go
@@ -0,0 +1,17 @@
+package main
+
+func spiralOrder(matrix [][]int) []int {
+
+	for i:=0; i<len(matrix[0]); i++ {
+		if i<len(matrix){
+			for j:= 0; j<len(matrix[0]) j++{
+				fmt.Print(matrix[i][j])
+			} 
+		}
+        
+    }
+}
+
+func main(){
+
+}
