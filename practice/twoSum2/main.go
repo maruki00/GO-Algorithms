111cc431a4cc63e1ded5a2e7f306339fcd250453
maruki00 <aitbaha48@gmail.com>
2024-08-26 22:52:40 +0300

    --

--- practice/twoSum2/main.go
+++ practice/twoSum2/main.go
@@ -1,8 +1,10 @@
 package main
 
+import "fmt"
+
 func twoSum(numbers []int, target int) []int {
 
-	l, r := 0, len(numbers)
+	l, r := 0, len(numbers)-1
 
 	for l < r {
 		if numbers[l]+numbers[r] == target {
@@ -19,5 +21,6 @@ func twoSum(numbers []int, target int) []int {
 }
 
 func main() {
-
+	nums := []int{2, 7, 11, 15}
+	fmt.Println("result : ", twoSum(nums, 9))
 }
