f61a8f973aa18e594879abcad0693249f473d8ba
maruki00 <aitbaha48@gmail.com>
2024-08-28 19:50:19 +0300

    --

---
+++ practice/middleOTheLinkedList/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+
+	low := head
+	height := head
+
+	lowCount := 0
+	hieghtCount := 0
+
+	for height != nil {
+		for lowCount < int(math.Ceil(float64(hieghtCount/2))) {
+			low = low.Next
+			lowCount++
+		}
+
+		height = head.Next
+		hieghtCount++
+
+	}
+
+	return low
+}
+
+func printList(l *ListNode) {
+	head := l
+	for head != nil {
+		fmt.Println("val : ", head.Val)
+		head = head.Next
+	}
+}
+
+func main() {
+	l := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 4,
+			Next: &ListNode{
+				Val: 6,
+				Next: &ListNode{
+					Val:  3,
+					Next: nil,
+				},
+			},
+		},
+	}
+
+	l = middleNode(l)
+
+	printList(l)
+
+}
