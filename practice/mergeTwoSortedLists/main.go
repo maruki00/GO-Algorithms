a5b4abab589e1239f4ed69e689bb23c790f4b497
maruki00 <aitbaha48@gmail.com>
2024-08-28 18:41:05 +0300

    --

---
+++ practice/mergeTwoSortedLists/main.go
@@ -0,0 +1,14 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	var list *ListNode
+}
+
+func main() {
+
+}
