7ada2f65939ef896063d4e20bb5c4c8d0d837bdc
maruki00 <aitbaha48@gmail.com>
2024-09-03 20:50:50 +0300

    --

--- practice/maximumAverageSubarray1/main.go
+++ practice/maximumAverageSubarray1/main.go
@@ -2,30 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func findMaxAverage(nums []int, k int) float64 {
-	res := float64(math.MinInt64)
 
 	prev := float64(0)
 
 	for i := 0; i < k; i++ {
 		prev += float64(nums[i])
 	}
+	res := prev / float64(k)
+	l, h := 0, k+1
 
-	for i := 0; i < len(nums); i++ {
-		if i+k > len(nums) {
-			break
-		}
-		tmp := float64(0)
-		for j := 0; j < k; j++ {
-			tmp += float64(nums[i+j])
-		}
-		res = max(res, tmp/float64(k))
-
+	for h < len(nums) {
+		prev -= float64(nums[l-1])
+		prev += float64(nums[h])
+		res = min(res, prev/float64(k))
 	}
-
 	return res
 }
 
