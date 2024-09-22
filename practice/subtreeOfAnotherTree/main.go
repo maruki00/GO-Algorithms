be9d9b0d7ad1335bee09316d8ad429b8d611399e
maruki00 <aitbaha48@gmail.com>
2024-09-22 04:49:47 +0300

    --

--- practice/subtreeOfAnotherTree/main.go
+++ practice/subtreeOfAnotherTree/main.go
@@ -1,33 +1,73 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-func isValidSubTree(root *TreeNode, subRoot *TreeNode) bool {
-	if root == nil && subRoot == nil {
+func isValidSubTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
 		return true
 	}
-
-	if root != nil || subRoot != nil || (root.Val != subRoot.Val) {
+	if p == nil || q == nil || p.Val != q.Val {
 		return false
 	}
-	return isValidSubTree(root.Left, subRoot.Left) && isValidSubTree(root.Right, subRoot.Right)
+	return isValidSubTree(p.Left, q.Left) && isValidSubTree(p.Right, q.Right)
 }
 
 func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
 	if root == nil {
-		return false
+		return nil == subRoot
 	}
 
-	if root.Val == subRoot.Val {
-		return isSubtree(root, subRoot)
+	if isValidSubTree(root, subRoot) {
+		return true
 	}
 
-	return isSubtree(root.Left, subRoot.Left) && isSubtree(root.Right, subRoot.Right)
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+
 }
 func main() {
+
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 4,
+			Left: &TreeNode{
+				Val:   1,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val:   2,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+		Right: &TreeNode{
+			Val:   5,
+			Left:  nil,
+			Right: nil,
+		},
+	}
+
+	sub := &TreeNode{
+		Val: 4,
+		Left: &TreeNode{
+			Val:   1,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+	}
+
+	fmt.Println("result : ", isSubtree(root, sub))
 
 }
