ff6d319b4a2e878cb297d07255e2f93165e69414
maruki00 <aitbaha48@gmail.com>
2025-01-03 22:17:53 +0100

    --

--- practice/findIfPathExistsInGraph/main.go
+++ practice/findIfPathExistsInGraph/main.go
@@ -1,33 +1,32 @@
 package main
 
-type Stack []int
-
-func (s Stack) pop() int {
-	last := s[len(s)-1]
-	s = s[:len(s)-1]
-	return last
-}
-
-func (s Stack) push(item ...int) {
-	s = append(s, item...)
-}
+import "fmt"
 
 func validPath(n int, edges [][]int, source int, destination int) bool {
+
+	stack := []int{source}
 	visited := make(map[int]bool, 0)
 	graph := make(map[int][]int, 0)
 	for _, a := range edges {
 		graph[a[0]] = append(graph[a[0]], a[1])
+		graph[a[1]] = append(graph[a[1]], a[0])
 	}
+	for len(stack) != 0 {
+		lst := stack[len(stack)-1]
+		if lst == destination {
+			return true
+		}
+		stack = stack[:len(stack)-1]
 
-	var BFS func(visited map[int]bool, node int)
-	BFS = func(visited map[int]bool, node int) {
-
+		if !visited[lst] {
+			visited[lst] = true
+			stack = append(stack, graph[lst]...)
+		}
 	}
-
-	BFS(visited, 0)
 	return false
 }
 
 func main() {
-	validPath(1, nil, 0, 0)
+	items := [][]int{{0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}}
+	fmt.Println(validPath(1, items, 0, 5))
 }
