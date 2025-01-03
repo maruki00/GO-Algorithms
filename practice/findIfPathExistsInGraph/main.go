26898059d87541864d125d5e749d7398e56b6069
maruki00 <aitbaha48@gmail.com>
2025-01-03 21:21:51 +0100

    --

--- practice/findIfPathExistsInGraph/main.go
+++ practice/findIfPathExistsInGraph/main.go
@@ -2,7 +2,11 @@ package main
 
 func validPath(n int, edges [][]int, source int, destination int) bool {
 	visited := make(map[[2]int]bool)
-	var dfs func()
+	var BFS func()
+	BFS = func(visited) {
+
+	}
+
 }
 
 func main() {
