dac062e5fcace0d2c1aeca04d3bf9d21a44e2678
maruki00 <aitbaha48@gmail.com>
2025-01-04 14:05:47 +0100

    --

--- practice/lastStoneWeight-1046/main.go
+++ practice/lastStoneWeight-1046/main.go
@@ -5,47 +5,46 @@ import (
 	"fmt"
 )
 
+func lastStoneWeight(stones []int) int {
+	maxHeap := &MaxHeap{}
+
+	for _, stone := range stones {
+		heap.Push(maxHeap, stone)
+	}
+
+	for maxHeap.Len() > 1 {
+		stone1 := heap.Pop(maxHeap).(int)
+		stone2 := heap.Pop(maxHeap).(int)
+
+		if stone1 != stone2 {
+			heap.Push(maxHeap, stone1-stone2)
+		}
+	}
+
+	res := 0
+	if maxHeap.Len() == 1 {
+		res = (*maxHeap)[0]
+	}
+	return res
+}
+
 type MaxHeap []int
 
-func (h MaxHeap) Len() int { return len(h) }
-
+func (h MaxHeap) Len() int           { return len(h) }
 func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h MaxHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
-
-func (h *MaxHeap) Push(x any) {
+func (h *MaxHeap) Push(x interface{}) {
 	*h = append(*h, x.(int))
 }
 
-func (h *MaxHeap) Pop() any {
-
-	// lst :=
-
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
+func (h *MaxHeap) Pop() interface{} {
+	x := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
 	return x
 }
 
 func main() {
-
-	h := &MaxHeap{}
-	heap.Init(h)
-
-	// Add elements to the heap
-	heap.Push(h, 10)
-	heap.Push(h, 5)
-	heap.Push(h, 20)
-
-	// Peek the largest element
-	fmt.Println("Largest element:", (*h)[0])
-
-	// Remove elements from the heap
-	fmt.Println("Popped element:", heap.Pop(h))
-	heap.Push(h, 67)
-
-	fmt.Println("Popped element:", h)
-	fmt.Println("Popped element:", heap.Pop(h))
-	fmt.Println("Popped element:", heap.Pop(h))
+	stones := []int{2, 7, 4, 1, 8, 1}
+	fmt.Println("result : ", lastStoneWeight(stones))
 }
