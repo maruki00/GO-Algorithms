37bf43a72c4fcf31bc53f348e87ed4219442a115
maruki00 <aitbaha48@gmail.com>
2025-01-08 20:35:34 +0100

    --

--- practice/KClosestPointsToOrgin-973/main.go
+++ practice/KClosestPointsToOrgin-973/main.go
@@ -1,26 +1,49 @@
 package main
 
-import "math"
+import (
+	"container/heap"
+	"math"
+)
 
 type Heap [][]int
 
 func (h Heap) Len() int { return len(h) }
-func (h Heap) Less(a, b []int) bool {
-	x1, x2 := math.Pow(float64(a[0]), 2), math.Pow(float64(b[0]), 2)
-	y1, y2 := math.Pow(float64(a[1]), 2), math.Pow(float64(b[1]), 2)
+func (h Heap) Less(a, b int) bool {
+	x1, x2 := math.Pow(float64(h[a][0]), 2), math.Pow(float64(h[b][0]), 2)
+	y1, y2 := math.Pow(float64(h[a][1]), 2), math.Pow(float64(h[b][1]), 2)
 	return math.Sqrt(x1+y1) > math.Sqrt(x2+y2)
 }
 func (h Heap) Swap(a, b int) {
 	h[a], h[b] = h[b], h[a]
 }
-func (h *Heap) Pop() []int {
+
+func (h *Heap) Pop() interface{} {
 	old := *h
-	lst := *old[len(*h)-1]
+	lst := old[len(*h)-1]
 	old = old[:len(*h)-1]
+	return lst
 }
+
+func (h *Heap) Push(x interface{}) {
+	*h = append(*h, x.([]int))
+}
+
+// func (h *Heap) Push(x interface{}) { *h = append(*h, x.([]int)) }
+// func (h *Heap) Pop() interface{} {
+// 	old := *h
+// 	lst := old[len(old)-1]
+// 	old = old[:len(old)-1]
+// 	*h = old
+// 	return lst
+// }
 
 func kClosest(points [][]int, k int) [][]int {
 
+	h := &Heap{}
+	heap.Init(h)
+	for _, item := range points {
+		heap.Push(h, item)
+	}
 	return [][]int{}
 }
 
