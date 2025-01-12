7dd3ba8ee06d7a748b063f7d55ff1a4005d3c5bc
maruki00 <aitbaha48@gmail.com>
2025-01-12 22:38:12 +0100

    --

--- practice/excelSheetColumnTitle-168/main.go
+++ practice/excelSheetColumnTitle-168/main.go
@@ -1,9 +1,23 @@
 package main
 
-func convertToTitle(columnNumber int) string {
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func reverse(b []byte) {
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+
+		b[i], b[j] = b[j], b[i]
+	}
 
 }
 
+func convertToTitle(n int) string {
+	var b []byte
+	for ; n > 0; n = (n - 1) / 26 {
+		b = append(b, letters[(n-1)%26])
+	}
+	reverse(b)
+	return string(b)
+}
 func main() {
 
 }
