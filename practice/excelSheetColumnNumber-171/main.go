49ba838408df2345869d917f52a760875525b951
maruki00 <aitbaha48@gmail.com>
2025-01-14 02:34:06 +0100

    --

--- practice/excelSheetColumnNumber-171/main.go
+++ practice/excelSheetColumnNumber-171/main.go
@@ -1,17 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func titleToNumber(columnTitle string) int {
-	chars := map[byte]int{
+	chars := map[rune]int{
 		'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10,
 		'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19,
 		'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
 	result := 0
-	for i, char := range columnTitle {
-		fmt.Println(char)
+	poow := len(columnTitle) - 1
+	for _, char := range columnTitle {
+		tmp := math.Pow(float64(26), float64(poow)) * float64(chars[char])
+		result += int(tmp)
+		poow--
 	}
-
 	return result
 }
 
