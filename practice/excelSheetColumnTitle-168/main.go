313714290391d2dbc676e1653bd8ed2dbbf1aad5
maruki00 <aitbaha48@gmail.com>
2025-01-14 03:18:02 +0100

    --

--- practice/excelSheetColumnTitle-168/main.go
+++ practice/excelSheetColumnTitle-168/main.go
@@ -1,23 +1,18 @@
 package main
 
+import "fmt"
+
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func reverse(b []byte) {
-	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
-
-		b[i], b[j] = b[j], b[i]
-	}
-
-}
-
 func convertToTitle(n int) string {
-	var b []byte
-	for ; n > 0; n = (n - 1) / 26 {
-		b = append(b, letters[(n-1)%26])
+	result := ""
+	for n > 0 {
+		fmt.Println(int((n - 1) % 26))
+		result = string(letters[int((n-1)%26)]) + result
+		n = (n - 1) / 26
 	}
-	reverse(b)
-	return string(b)
+	return result
 }
 func main() {
-
+	fmt.Println("result : ", convertToTitle(701))
 }
