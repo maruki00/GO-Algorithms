8affd5813b91e61169f5a017e0d755dcc369d9cc
maruki00 <aitbaha48@gmail.com>
2025-01-14 20:07:48 +0100

    --

--- practice/reverseBits-190/main.go
+++ practice/reverseBits-190/main.go
@@ -1,12 +1,19 @@
 package main
 
+import "fmt"
+
 func reverseBits(num uint32) uint32 {
 	result := uint32(0)
 	for num > 0 {
-		result += uint32((uint32(result) * 10) + (num % 10))
+		fmt.Println(num, result)
+		result = uint32((result * 10) + (num % 10))
 		num /= 10
 	}
 	return result
 }
 
-func main() {}
+func main() {
+
+	y := 43261596
+	fmt.Println(reverseBits(uint32(y)))
+}
