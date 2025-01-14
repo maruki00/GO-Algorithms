06c8fdea5b48ff39d26abd73377740057e13c0a0
maruki00 <aitbaha48@gmail.com>
2025-01-14 21:10:58 +0100

    --

--- practice/happyNumber-202/main.go
+++ practice/happyNumber-202/main.go
@@ -9,15 +9,14 @@ func isHappy(n int) bool {
 	if n == 1 {
 		return true
 	}
-	if n < 10 {
+	if n < 10 && n%2 == 0 {
 		return false
 	}
 	num := 0
 	for n > 0 {
-		num = num*10 + int(math.Pow(float64(num%10), float64(2)))
+		num += int(math.Pow(float64(n%10), float64(2)))
 		n /= 10
 	}
-	fmt.Println(n, num)
 	return isHappy(num)
 }
 
