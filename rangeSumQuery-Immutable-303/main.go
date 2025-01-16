9affb8603843bfd81da84d995e016c65092fddf2
maruki00 <aitbaha48@gmail.com>
2025-01-17 00:24:47 +0100

    --

--- rangeSumQuery-Immutable-303/main.go
+++ rangeSumQuery-Immutable-303/main.go
@@ -3,30 +3,32 @@ package main
 import "fmt"
 
 type NumArray struct {
-	nums []int
+	nums  []int
+	cache []int
 }
 
 func Constructor(nums []int) NumArray {
-	return NumArray{
-		nums: nums,
+	obj := NumArray{
+		nums:  nums,
+		cache: make([]int, len(nums)+1),
 	}
+	obj.cache[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		obj.cache[i] = obj.cache[i-1] + nums[i]
+	}
+	return obj
 }
 
 func (this *NumArray) SumRange(left int, right int) int {
 	if left == right {
 		return this.nums[left]
 	}
-	sumNum := 0
-	for left < right {
-		fmt.Println("item : ", left, right, sumNum)
-		sumNum += this.nums[left] + this.nums[right]
-		left++
-		right--
-	}
-	return sumNum
+	return this.cache[right] - this.cache[left] + this.nums[left]
 }
 
 func main() {
+
 	nums := Constructor([]int{1, 4, -6})
+	fmt.Println(nums)
 	fmt.Println("result : ", nums.SumRange(0, 2))
 }
