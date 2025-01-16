809e3fb2dca66b6045301ffb2b81a07befb1df59
maruki00 <aitbaha48@gmail.com>
2025-01-16 23:30:27 +0100

    --

--- practice/main.go
+++ practice/main.go
@@ -5,22 +5,19 @@ import "fmt"
 func moveZeroes(nums []int) {
 	i, j := 0, 0
 	for i < len(nums) {
-		if nums[i] != 0 {
-			i++
-		}
 		if nums[j] != 0 {
 			j++
 		}
 		if nums[i] != 0 && nums[j] == 0 {
 			nums[i], nums[j] = nums[j], nums[i]
 			j++
-			i++
 		}
+		i++
 	}
 }
 
 func main() {
 	nums := []int{0, 1, 0, 3, 12}
-	moveZeros(nums)
+	moveZeroes(nums)
 	fmt.Println("result : ", nums)
 }
