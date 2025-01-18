f768b4f4d5c093a0d781d7d5dd2224b78adba2b9
maruki00 <aitbaha48@gmail.com>
2025-01-18 16:58:17 +0100

    --

--- practice/power-of-two-231/main.go
+++ practice/power-of-two-231/main.go
@@ -1,11 +1,11 @@
 package main
 
-import "math"
+import "fmt"
 
 func isPowerOfTwo(n int) bool {
 	res := n & (n - 1)
 	return res == 0 && n != 0
 }
 func main() {
-
+	fmt.Println("result : ", isPowerOfTwo(12322345234))
 }
