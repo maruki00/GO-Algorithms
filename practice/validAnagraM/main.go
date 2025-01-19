9c6b1bb78786b66feecca38a0089bee4322d35e9
maruki00 <aitbaha48@gmail.com>
2025-01-19 12:16:56 +0100

    --

--- practice/validAnagraM/main.go
+++ practice/validAnagraM/main.go
@@ -3,8 +3,26 @@ package main
 import (
 	"fmt"
 )
-
 func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	hashMap := make(map[rune]int)
+	for _, j := range s {
+		hashMap[j]++
+	}
+	for _, j := range t {
+		hashMap[j]--
+	}
+	for _, j := range hashMap {
+		if j != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func _isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
