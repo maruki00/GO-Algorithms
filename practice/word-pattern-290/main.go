69b322816e35e761a6cad44ed48506224e6edf01
maruki00 <aitbaha48@gmail.com>
2025-01-19 13:31:29 +0100

    --

--- practice/word-pattern-290/main.go
+++ practice/word-pattern-290/main.go
@@ -10,6 +10,25 @@ func wordPattern(pattern string, s string) bool {
 	if len(pattern) != len(sArray) {
 		return false
 	}
+	sMap := make(map[byte]string)
+	tMap := make(map[string]string)
+	for i := 0; i < len(pattern); i++ {
+		item, ok := sMap[pattern[i]]
+		item1, ok1 := tMap[string(sArray[i])]
+		if (ok || ok1) && (item != string(sArray[i])) && (string(pattern[i]) != item1) {
+			return false
+		}
+		sMap[pattern[i]] = sArray[i]
+		tMap[sArray[i]] = string(pattern[i])
+	}
+	return true
+}
+
+func _wordPattern(pattern string, s string) bool {
+	sArray := strings.Split(s, " ")
+	if len(pattern) != len(sArray) {
+		return false
+	}
 	sMap := make(map[string]string)
 	tMap := make(map[string]string)
 	for i := 0; i < len(pattern); i++ {
