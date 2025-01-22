68634d0f338ec0ec26384a4698803cc96d7cb29a
maruki00 <aitbaha48@gmail.com>
2025-01-22 22:24:31 +0100

    --

--- practice/add-strings-415/main.go
+++ practice/add-strings-415/main.go
@@ -1,18 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func addStrings(num1 string, num2 string) string {
-	n1, n2 := int64(0), int64(0)
-	for _, nn1 := range num1 {
-		n1 = int64((n1 * 10) + int64(nn1-'0'))
+	l1, l2 := len(num1)-1, len(num2)-1
+	result := ""
+	l := 1
+	remain := 0
+	tmp := 0
+	for l1 >= 0 && l2 >= 0 {
+		tmp = int(num1[l1]-'0') + int(num2[l2]-'0')
+		result = string((tmp+remain)%10+'0') + result
+		remain = (remain + tmp) / 10
+
+		fmt.Println(tmp, int(num1[l1]-'0'), int(num2[l2]-'0'), remain, result)
+		l1--
+		l2--
+		l *= 10
+
 	}
-	for _, nn1 := range num2 {
-		n2 = int64((n2 * 10) + int64(nn1-'0'))
+	for l1 >= 0 {
+		tmp = int(num1[l1] - '0')
+		result = string((tmp+remain)%10+'0') + result
+		remain = (remain + tmp) / 10
+		l1--
+		l *= 10
 	}
-	return fmt.Sprintf("%d", n1+n2)
+	for l2 >= 0 {
+		tmp = int(num2[l2] - '0')
+		result = string((tmp+remain)%10+'0') + result
+		remain = (remain + tmp) / 10
+		l2--
+		l *= 10
+	}
+	return result
+	//return fmt.Sprintf("%s", result)
+}
+
+func _addStrings(a string, b string) string {
+	i := big.NewInt(0)
+	j := big.NewInt(0)
+	i.SetString(a, 10)
+	j.SetString(b, 10)
+	res := i.Add(i, j)
+	return fmt.Sprintf("%v", res)
 }
 
 func main() {
-	fmt.Println(addStrings("123", "11"))
+	fmt.Println(addStrings("3876620623801494171", "6529364523802684779"))
 }
