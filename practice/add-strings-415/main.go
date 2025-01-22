34e64911ed9f02922a659892dcda7dd8366ada15
maruki00 <aitbaha48@gmail.com>
2025-01-22 21:34:12 +0100

    --

--- practice/add-strings-415/main.go
+++ practice/add-strings-415/main.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func addStrings(num1 string, num2 string) string {
+	n1, n2 := 0, 0
+	for _, nn1 := range num1 {
+		n1 = (n1 * 10) + int(nn1-'0')
+	}
+	for _, nn1 := range num2 {
+		n2 = (n2 * 10) + int(nn1-'0')
+	}
+	return "123"
+}
+
+func main() {
+	fmt.Println(addStrings("123", "11"))
+}
