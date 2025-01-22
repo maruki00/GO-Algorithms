7e2d2ee16c2d3308318d932a4be03851c9854027
maruki00 <aitbaha48@gmail.com>
2025-01-22 21:41:19 +0100

    --

--- practice/add-strings-415/main.go
+++ practice/add-strings-415/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func addStrings(num1 string, num2 string) string {
-	n1, n2 := 0, 0
+	n1, n2 := int64(0), int64(0)
 	for _, nn1 := range num1 {
-		n1 = (n1 * 10) + int(nn1-'0')
+		n1 = int64((n1 * 10) + int64(nn1-'0'))
 	}
 	for _, nn1 := range num2 {
-		n2 = (n2 * 10) + int(nn1-'0')
+		n2 = int64((n2 * 10) + int64(nn1-'0'))
 	}
 	return fmt.Sprintf("%d", n1+n2)
 }
