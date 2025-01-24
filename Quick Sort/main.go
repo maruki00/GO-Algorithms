d8d2a1a795ddc9f404f32bb9a2d10a3bb105fb6f
maruki00 <aitbaha48@gmail.com>
2025-01-24 16:11:06 +0100

    --

--- Quick Sort/main.go
+++ Quick Sort/main.go
@@ -1,9 +1,45 @@
 package main
 
-func QuickSort(nums []int) []int {
+import "fmt"
 
+func QuickSort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	pivot := nums[len(nums)-1]
+	left := []int{}
+	right := []int{}
+	for _, n := range nums[:len(nums)-1] {
+		if n > pivot {
+			right = append(right, n)
+			continue
+		}
+		left = append(left, n)
+	}
+	return append(append(QuickSort(left), pivot), QuickSort(right)...)
+}
+
+func QuickSort_(arr []int) []int {
+
+	if len(arr) <= 1 {
+		return arr
+	}
+	left := []int{}
+	right := []int{}
+
+	pivot := arr[len(arr)-1]
+	for _, item := range arr[:len(arr)-1] {
+		if item > pivot {
+			right = append(right, item)
+			continue
+		}
+		left = append(left, item)
+	}
+	return append(append(QuickSort(left), pivot), QuickSort(right)...)
 }
 
 func main() {
-
+	nums := []int{1, 2, 8, 4, 2, 8, 9, 1}
+	fmt.Println(QuickSort(nums))
 }
