d5fff8a6321da89db31ced3341c49bb881fb7405
maruki00 <aitbaha48@gmail.com>
2025-01-25 21:01:24 +0100

    --

---
+++ practice/nim-game-292/main.go
@@ -0,0 +1,9 @@
+package main
+
+func canWinNim(n int) bool {
+	return n%4 != 0
+}
+
+func main() {
+
+}
--- practice/third-maximum-number-414/main.go
+++ practice/third-maximum-number-414/main.go
@@ -5,24 +5,33 @@ import (
 	"sort"
 )
 
+type Set []int
+
+func InitSet(nums []int) *Set {
+	s := make(Set, 0)
+	_set := make(map[int]bool)
+	for _, n := range nums {
+		_set[n] = true
+	}
+	for n, _ := range _set {
+		s = append(s, n)
+	}
+	return &s
+}
+
 func thirdMax(nums []int) int {
-	sort.Ints(nums)
-	prev := nums[0]
-	index := 1
-	if len(nums) <= 2 {
-		return nums[len(nums)-1]
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] > nums[j]
+	})
+	if len(nums) < 2 {
+		return nums[0]
 	}
-	for i := 0; i < len(nums); i++ {
-		if prev == nums[i] {
-			continue
-		}
-		prev = nums[i-1]
-		if index >= 3 {
-			return nums[i]
-		}
-		index++
+	set := InitSet(nums)
+	old := *set
+	if len(old) > 2 {
+		return old[2]
 	}
-	return nums[len(nums)-1]
+	return old[len(old)-1]
 }
 func main() {
 	nums := []int{2, 2, 3, 1}
