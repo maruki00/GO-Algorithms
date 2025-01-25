fe70c6346e5c5309762c412aea696bb00e2e5d9b
maruki00 <aitbaha48@gmail.com>
2025-01-25 13:18:21 +0100

    --

--- practice/third-maximum-number-414/main.go
+++ practice/third-maximum-number-414/main.go
@@ -1,15 +1,30 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"sort"
+)
 
 func thirdMax(nums []int) int {
 	sort.Ints(nums)
-	if len(nums) > 2 {
-		return nums[2]
+	prev := nums[0]
+	index := 1
+	if len(nums) <= 2 {
+		return nums[len(nums)-1]
+	}
+	for i := 0; i < len(nums); i++ {
+		if prev == nums[i] {
+			continue
+		}
+		prev = nums[i-1]
+		if index >= 3 {
+			return nums[i]
+		}
+		index++
 	}
 	return nums[len(nums)-1]
 }
-
 func main() {
-
+	nums := []int{2, 2, 3, 1}
+	fmt.Println(thirdMax(nums))
 }
