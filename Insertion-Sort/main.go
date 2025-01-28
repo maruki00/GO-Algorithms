627f1d371e4a5441d47ae12a9a77494c8a52a448
maruki00 <aitbaha48@gmail.com>
2025-01-28 17:58:05 +0100

    --

--- Insertion-Sort/main.go
+++ Insertion-Sort/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func Sort(nums []int) {
 	for i := 1; i < len(nums); i++ {
-		for j := i; nums[j] < nums[j-1] && j > 0; j-- {
+		for j := i; j > 0 && nums[j] < nums[j-1]; j-- {
 			nums[j], nums[j-1] = nums[j-1], nums[j]
 		}
 	}
