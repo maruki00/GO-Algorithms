423f52b9d4c17aedbcc503f749abd15ea865878e
maruki00 <aitbaha48@gmail.com>
2025-01-28 17:56:57 +0100

    --

---
+++ Insertion-Sort/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Sort(nums []int) {
+	for i := 1; i < len(nums); i++ {
+		for j := i; nums[j] < nums[j-1] && j > 0; j-- {
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+		}
+	}
+}
+
+func main() {
+	nums := []int{1, 5, 3, 6, 3, 8, 3, -1, 7, 3, 7}
+	Sort(nums)
+	fmt.Println("result : ", nums)
+}
