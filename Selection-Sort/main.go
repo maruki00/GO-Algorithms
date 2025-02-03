e548168785def0aac25aaa268b705bf56a12a0fe
maruki00 <aitbaha48@gmail.com>
2025-02-03 18:38:32 +0100

    --

--- Selection-Sort/main.go
+++ Selection-Sort/main.go
@@ -1,18 +1,22 @@
 package main
 
+import "fmt"
+
 func Sort(nums []int) {
 
 	for i := 0; i < len(nums); i++ {
 		min_index := i
 		for j := i + 1; j < len(nums); j++ {
-			if nums[i] > nums[min_index] {
+			if nums[i] > nums[j] {
 				min_index = j
 			}
 		}
+		nums[min_index], nums[i] = nums[i], nums[min_index]
 	}
-
 }
 
 func main() {
-
+	nums := []int{1, 5, 7, 3, -1, 5, 0, 102}
+	Sort(nums)
+	fmt.Println("result : ", nums)
 }
