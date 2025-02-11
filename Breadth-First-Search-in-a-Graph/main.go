09a1cd43592eb74af54fbef488cac4412643a5cb
maruki00 <aitbaha48@gmail.com>
2025-02-11 13:28:40 +0100

    --

--- Breadth-First-Search-in-a-Graph/main.go
+++ Breadth-First-Search-in-a-Graph/main.go
@@ -1,14 +1,49 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Node struct {
-	node int
-	sub  []int
+	node      int
+	adjancies []int
 }
 
 type Graph struct {
-	nodes []*Node
+	adjancies map[byte][]byte
 }
 
-func main() {
+func (g *Graph) AddAdjancy(node, adj byte) {
+	for _, nd := range g.adjancies[node] {
+		if nd == adj {
+			return
+		}
+	}
+	g.adjancies[node] = append(g.adjancies[node], adj)
+}
 
+func DFS(start byte, g *Graph) {
+	visited := make(map[byte]bool, len(g.adjancies))
+	queue := []byte{start}
+	fmt.Println(queue, visited)
+}
+
+func (g *Graph) Print() {
+	for node, vals := range g.adjancies {
+		fmt.Print("Node [", string(node), "] : ")
+		for _, item := range vals {
+			print(string(item), ", ")
+		}
+		fmt.Println("")
+	}
+}
+func main() {
+	g := &Graph{
+		adjancies: make(map[byte][]byte),
+	}
+	g.AddAdjancy('a', 'b')
+	g.AddAdjancy('b', 'b')
+	g.AddAdjancy('c', 'b')
+	g.AddAdjancy('a', 'c')
+	g.Print()
 }
