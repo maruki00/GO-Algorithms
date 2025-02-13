cc64518897384d439437f9fa7e5cec8d7483c50a
maruki00 <aitbaha48@gmail.com>
2025-02-13 17:42:53 +0100

    --

--- practice/n-th-tribonacci-number-1137/main.go
+++ practice/n-th-tribonacci-number-1137/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func tribonacci(n int) int {
-	tracking := make([]int, n+10)
+	tracking := make([]int, n+3)
 	tracking[0] = 0
 	tracking[1] = 1
 	tracking[2] = 1
-	for i := 0; i <= n; i++ {
-		tracking[i+3] = tracking[i] + tracking[i+1] + tracking[i+2]
+	for i := 3; i <= n; i++ {
+		tracking[i] = tracking[i-1] + tracking[i-2] + tracking[i-3]
 	}
 	return tracking[n]
 }
