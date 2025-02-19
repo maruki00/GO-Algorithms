8b1dc6c2f96c7a140883ad35cd1b0bed12d1b094
maruki00 <aitbaha48@gmail.com>
2025-02-19 13:30:34 +0100

    --

--- practice/third-maximum-number-414/main.go
+++ practice/third-maximum-number-414/main.go
@@ -5,35 +5,107 @@ import (
 	"sort"
 )
 
-type Set []int
+// type MaxHeap []int
 
-func InitSet(nums []int) *Set {
-	s := make(Set, 0)
-	_set := make(map[int]bool)
-	for _, n := range nums {
-		_set[n] = true
+// func (h MaxHeap) Len() int {
+// 	return len(h)
+// }
+
+// func (h MaxHeap) Less(i, j int) bool {
+// 	return h[i] < h[j]
+// }
+
+// func (h MaxHeap) Swap(i, j int) {
+// 	h[i], h[j] = h[j], h[i]
+// }
+
+// func (h *MaxHeap) Push(x interface{}) {
+// 	*h = append(*h, x.(int))
+// }
+
+// func (h *MaxHeap) Pop() interface{} {
+// 	old := *h
+// 	n := len(old)
+// 	x := old[n-1]
+// 	*h = old[:n-1]
+// 	return x
+// }
+
+// func buildHeapByInit(array []int) *MaxHeap {
+// 	maxHeap := &MaxHeap{}
+// 	nums := make([]int, 0)
+// 	tracking := make(map[int]bool)
+// 	for _, i := range array {
+// 		if _, ok := tracking[i]; ok {
+// 			continue
+// 		}
+// 		tracking[i] = true
+// 		nums = append(nums, i)
+
+// 	}
+// 	fmt.Println(nums)
+// 	*maxHeap = nums
+// 	heap.Init(maxHeap)
+// 	fmt.Println("buildHeapByInit: ", *maxHeap)
+// 	return maxHeap
+// }
+
+// func thirdMax(nums []int) int {
+// 	fmt.Println(nums)
+// 	mHeap := buildHeapByInit(nums)
+
+// 	ret := 0
+// 	for mHeap.Len() > 0 {
+// 		ret = mHeap.Pop().(int)
+// 		fmt.Println(ret)
+// 		if mHeap.Len() == 0 {
+// 			return ret
+// 		}
+// 	}
+
+//		return ret
+//	}
+
+func SetFromInts(nums []int) []int {
+	tracking := make(map[int]bool)
+	s := []int{}
+	for _, i := range nums {
+		if _, ok := tracking[i]; ok {
+			continue
+		}
+		tracking[i] = true
+		s = append(s, i)
 	}
-	for n, _ := range _set {
-		s = append(s, n)
+	sort.Slice(s, func(i, j int) bool {
+		return s[i] > s[j]
+	})
+	fmt.Println(s)
+	return s
+}
+
+func Len(s []int) int {
+	return len(s)
+}
+
+func At(s []int, i int) int {
+	if Len(s) == 0 {
+		return 0
 	}
-	return &s
+	return s[i]
 }
 
 func thirdMax(nums []int) int {
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] > nums[j]
-	})
-	if len(nums) < 2 {
-		return nums[0]
+	fmt.Println(nums)
+	s := SetFromInts(nums)
+
+	if Len(s) > 2 {
+		return At(s, 2)
 	}
-	set := InitSet(nums)
-	old := *set
-	if len(old) > 2 {
-		return old[2]
-	}
-	return old[len(old)-1]
+
+	return At(s, 0)
 }
+
 func main() {
 	nums := []int{2, 2, 3, 1}
-	fmt.Println(thirdMax(nums))
+	fmt.Println("result : ", thirdMax(nums))
 }
