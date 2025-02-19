0be4e8921d48c01604c7ee47194865207cef2373
maruki00 <aitbaha48@gmail.com>
2025-02-19 15:55:41 +0100

    --

--- practice/third-maximum-number-414/main.go
+++ practice/third-maximum-number-414/main.go
@@ -2,69 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
-// type MaxHeap []int
+func thirdMax_optimized(nums []int) int {
+	maxes := [3]int{
+		math.MinInt,
+		math.MinInt,
+		math.MinInt,
+	}
 
-// func (h MaxHeap) Len() int {
-// 	return len(h)
-// }
-
-// func (h MaxHeap) Less(i, j int) bool {
-// 	return h[i] < h[j]
-// }
-
-// func (h MaxHeap) Swap(i, j int) {
-// 	h[i], h[j] = h[j], h[i]
-// }
-
-// func (h *MaxHeap) Push(x interface{}) {
-// 	*h = append(*h, x.(int))
-// }
-
-// func (h *MaxHeap) Pop() interface{} {
-// 	old := *h
-// 	n := len(old)
-// 	x := old[n-1]
-// 	*h = old[:n-1]
-// 	return x
-// }
-
-// func buildHeapByInit(array []int) *MaxHeap {
-// 	maxHeap := &MaxHeap{}
-// 	nums := make([]int, 0)
-// 	tracking := make(map[int]bool)
-// 	for _, i := range array {
-// 		if _, ok := tracking[i]; ok {
-// 			continue
-// 		}
-// 		tracking[i] = true
-// 		nums = append(nums, i)
-
-// 	}
-// 	fmt.Println(nums)
-// 	*maxHeap = nums
-// 	heap.Init(maxHeap)
-// 	fmt.Println("buildHeapByInit: ", *maxHeap)
-// 	return maxHeap
-// }
-
-// func thirdMax(nums []int) int {
-// 	fmt.Println(nums)
-// 	mHeap := buildHeapByInit(nums)
-
-// 	ret := 0
-// 	for mHeap.Len() > 0 {
-// 		ret = mHeap.Pop().(int)
-// 		fmt.Println(ret)
-// 		if mHeap.Len() == 0 {
-// 			return ret
-// 		}
-// 	}
-
-//		return ret
-//	}
+	for _, num := range nums {
+		if maxes[0] == num || maxes[1] == num || maxes[2] == num {
+			continue
+		}
+		if num > maxes[0] || num == maxes[0] {
+			maxes[0], maxes[1], maxes[2] = num, maxes[0], maxes[1]
+			continue
+		}
+		if num > maxes[1] || num == maxes[2] {
+			maxes[1], maxes[2] = num, maxes[1]
+			continue
+		}
+		if num > maxes[2] || num == maxes[2] {
+			maxes[2] = num
+			continue
+		}
+	}
+	if maxes[2] == math.MinInt {
+		return maxes[0]
+	}
+	return maxes[2]
+}
 
 func SetFromInts(nums []int) []int {
 	tracking := make(map[int]bool)
@@ -79,7 +49,6 @@ func SetFromInts(nums []int) []int {
 	sort.Slice(s, func(i, j int) bool {
 		return s[i] > s[j]
 	})
-	fmt.Println(s)
 	return s
 }
 
