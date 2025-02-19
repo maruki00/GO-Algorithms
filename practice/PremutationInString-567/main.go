04c9dfd68fb103cd140ef339688878e73c278ebb
Abdellah Oulahyane <aitbaha48@gmail.com>
2025-02-19 17:04:44 +0100

    Add files via upload

--- practice/PremutationInString-567/main.go
+++ practice/PremutationInString-567/main.go
@@ -1,41 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
-
-func equals(s1, s2 string) bool {
-	if len(s1) != len(s2) {
-		return false
-	}
-	if s1 == s2 {
-		return true
-	}
-	hashMap := make(map[byte]int)
-	for i, _ := range s1 {
-		hashMap[s1[i]]++
-		hashMap[s2[i]]--
-	}
-	for _, item := range hashMap {
-		if item != 0 {
-			return false
-		}
-	}
-	return true
-}
+import "fmt"
 
 func checkInclusion(s1 string, s2 string) bool {
-	l := len(s1)
-	for i := 0; i < len(s2)-l+1; i++ {
-		if equals(s2[i:i+l], s1) {
+	for i := 0; i <= (len(s2) - len(s1)); i++ {
+		freq := make(map[byte]int)
+		for j := 0; j < len(s1); j++ {
+			freq[s2[i+j]]++
+			freq[s1[j]]--
+		}
+		res := true
+		for _, v := range freq {
+			if v != 0 {
+				res = false
+				break
+			}
+		}
+		if res {
 			return true
 		}
+
 	}
 	return false
 }
 
 func main() {
-	s1 := "ab"
-	s2 := "eidboaoo" //"eidbaooo"
+	s1 := "trfhcbogglim"
+	s2 := "amwfpqwfwkarvhfcisywzaahtbdbuicxmjseeoudwfcdxetbmacayfikolbdxkocezhalfhxabwvuddcyazwiqiwefgolzvrpdxcuskpsmwhslpeygjrvvajajafppcwkqhxwkigemfullbqkvuqwfnqnhxiltyfcpfdyumfwyelmtzbdccmbvxedgfimmsqwxmopvxmuonuzyzlhpeunailpydcqybghdwvqxrpautsvrhfxprdqlgzownvincoxjnjwrqrdgpegtgvlifbbautkfqbhqiftbmxadvorqjnqlsceuctazxgofmchypspqvwyzoeejqfkvvwftvagajafmafvytslubpzalnahjknarjswkxmzzlmlokrifiopjcamvynmmuegnzvveetssuucqclbzxgjwbsflyelpdsvzicdnlenuxggcsrckfdixsqcjrzsbztgvxbpktlbdqrcqoatgxqhwehqiuqjnldursyzplwlcdvwrmlknviqtexwgqovwbcdugdblakufxcapvkvhraacetowtcypfxlvwmwdafesfgqezspbvqzxicblrdsmmdzunpcmzvysgbnspuldkppwlrsrnnewwjquhzrodmsbgbycvrzmtnskyuqqoqkxyakojewbbtntbdjuncpgbwgrtvewyscyujnqtpuaulrnjqmdujxydwzfyqfnxmjqogibxqeuqdxsdjjpootpzmhcvoeyvdspktyjzadkfwcdulsuktottgpvptjfydvpdxoznzhbkmitaiywqklwrktmzsyndnqmtapaaadzkynfxiwqxtekcbkmcwhwwdylvoxosxcexeceavpfptdlkyinhdobrnxfdbtuomjojmzeytlntkundrydqmkiayounnbhfxrlriuatzumgfcyniicwhtsaffhnxamwjtgbxvewtgovvrjvblrlvrghyoiicgvyorzjgecmxqeiwpuubfrnkmpynwywqczqdpeinebgfyrhouvifthoaariadurpbrexbfnuwgkbmgowjuaysnmidptzetckscxvxttdogpywxdvaktmkispgyghfazxyxslyjhqorndzpjepmwiuisfhvacnpkthbfrasndrfkfuhpetlnfugmqhqpvtvlwumhxduxxmugstcbksvqholothhftzungtxdysudnixkzekpdlgddnvyfuitcedxvjfsjxhbcrenufafxzdrumeavumdbvvgpodgtsjzznxkpbfltchmogigordwcpcanomjznfmsxpzqgxigjpybooxsgyiuxskowkdpypnzpgebowqefomcpmfilixgzvoffvmcypgyrwhwaelfpclzaoldlaimtnszckziyqewrtewpfyhphxruytifwtodznvxmxwoibqvtmynpqshnmiymrayaenoiknjqzwoltqhaganjdwzkncathqrgcigaguimqgznupmsikurxjltfydqiozmddxydgtsvwoloqtlqhryfqmcsfetvtjkauyjgillobotqfhzsyjtcjsiqxhwoaucluagbltdwroayydlwzytpqlsxkbrgcavvaqvlggewskeflsejklqexjvcudzaanxrgnkwygokcuxkvypsh"
 	fmt.Println("result : ", checkInclusion(s1, s2))
 }
