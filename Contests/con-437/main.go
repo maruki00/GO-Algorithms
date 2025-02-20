5329bb357c0668c03d6424f5b0a353f3b6cb1c39
maruki00 <aitbaha48@gmail.com>
2025-02-20 12:05:36 +0100

    --

--- Contests/con-437/main.go
+++ Contests/con-437/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func hasSpecialSubstring(s string, k int) bool {
 
 	for i := 0; i <= len(s)-k; i++ {
-		l, r := i, i+k
+		l, r := i, i+k-1
 		conter := 0
 		for l <= r {
 			if s[l] != s[r] {
@@ -23,8 +23,7 @@ func hasSpecialSubstring(s string, k int) bool {
 }
 
 func main() {
-	s := "abc"
+	s := "aac"
 	k := 2
 	fmt.Println("result : ", hasSpecialSubstring(s, k))
 }
-
