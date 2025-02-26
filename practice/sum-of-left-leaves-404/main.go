83aaac4682b01676809a27278fc5da82fe39b7a7
maruki00 <aitbaha48@gmail.com>
2025-02-26 16:57:41 +0000

    --

--- practice/sum-of-left-leaves-404/main.go
+++ practice/sum-of-left-leaves-404/main.go
@@ -9,16 +9,37 @@ type TreeNode struct {
 }
 
 func sumOfLeftLeaves(root *TreeNode) int {
-	if root == nil {
-		return 0
+	res := 0
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		fmt.Println("node : ", root.Val)
+		if root.Left != nil && (root.Left.Left == nil && root.Left.Right == nil) {
+			res += root.Left.Val
+		}
+		sumOfLeftLeaves(root.Left)
+		sumOfLeftLeaves(root.Right)
 	}
-	fmt.Println("node : ", root.Val)
-	if root.Left != nil && (root.Left.Left == nil && root.Left.Right == nil) {
-		return root.Left.Val
-	}
-	return sumOfLeftLeaves(root.Left) + sumOfLeftLeaves(root.Right)
+	return res
 }
 
 func main() {
-
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	_ = sumOfLeftLeaves(root)
 }
