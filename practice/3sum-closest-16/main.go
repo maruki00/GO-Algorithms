9ffbc67d2b04f07f70e771bd1d5e96e232ed1269
maruki00 <aitbaha48@gmail.com>
2025-03-04 13:06:17 +0000

    --

---
+++ practice/3sum-closest-16/main.go
@@ -0,0 +1,33 @@
+package main
+
+func abs(num int) int {
+	if num < 0 {
+		return num * -1
+	}
+	return num
+}
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+func threeSumClosest(nums []int, target int) int {
+	resu := 0
+	i := 0
+	for ; i < 3 && i < len(nums); i++ {
+		resu += nums[i]
+	}
+	closeset := resu
+
+	for ; i < len(nums)-3; i++ {
+		resu -= nums[i]
+		resu += nums[i+3]
+		closeset = min(closeset, abs(target-resu))
+	}
+	return closeset
+}
+
+func main() {
+
+}
