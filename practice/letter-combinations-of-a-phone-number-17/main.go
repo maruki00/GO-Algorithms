096a22290c7155f52575e0d202076c8a76585a91
maruki00 <aitbaha48@gmail.com>
2025-03-07 23:04:14 +0000

    commit

--- practice/letter-combinations-of-a-phone-number-17/main.go
+++ practice/letter-combinations-of-a-phone-number-17/main.go
@@ -3,27 +3,28 @@ package main
 import "fmt"
 
 func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
 	result := []string{}
 	symboles := map[byte]string{
 		'2': "abc",
 		'3': "def",
-		'4': "jhi",
-		'5': "gkl",
+		'4': "ghi",
+		'5': "jkl",
 		'6': "mno",
 		'7': "pqrs",
 		'8': "tuv",
 		'9': "wxyz",
 	}
-	var recur func(syms string, tmp string)
-	recur = func(syms string, tmp string) {
-		if len(syms) == 0 {
+	var recur func(digits string, tmp string)
+	recur = func(digits string, tmp string) {
+		if len(digits) == 0 {
+			result = append(result, tmp)
 			return
 		}
-		for _, j := range symboles[syms[0]] {
-			tmp += string(j)
-			recur(syms[1:], tmp)
-			result = append(result, tmp)
-			fmt.Println(tmp)
+		for _, j := range symboles[digits[0]] {
+			recur(digits[1:], tmp+string(j))
 		}
 	}
 	recur(digits, "")
