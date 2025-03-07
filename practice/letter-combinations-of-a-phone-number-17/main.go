a615c634932a510505b44bb914ac8256ddf1b09c
maruki00 <aitbaha48@gmail.com>
2025-03-07 21:44:08 +0000

    commit

--- practice/letter-combinations-of-a-phone-number-17/main.go
+++ practice/letter-combinations-of-a-phone-number-17/main.go
@@ -1,9 +1,35 @@
 package main
 
-func letterCombinations(digits string) []string {
+import "fmt"
 
+func letterCombinations(digits string) []string {
+	result := []string{}
+	symboles := map[byte]string{
+		'2': "abc",
+		'3': "def",
+		'4': "jhi",
+		'5': "gkl",
+		'6': "mno",
+		'7': "pqrs",
+		'8': "tuv",
+		'9': "wxyz",
+	}
+	var recur func(syms string, tmp string)
+	recur = func(syms string, tmp string) {
+		if len(syms) == 0 {
+			return
+		}
+		for _, j := range symboles[syms[0]] {
+			tmp += string(j)
+			recur(syms[1:], tmp)
+			result = append(result, tmp)
+			fmt.Println(tmp)
+		}
+	}
+	recur(digits, "")
+	return result
 }
 
 func main() {
-
+	fmt.Println("result : ", letterCombinations("23"))
 }
