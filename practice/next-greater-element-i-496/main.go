21cb4a634e136f1e6271a6b53106df5eab7b114c
maruki00 <aitbaha48@gmail.com>
2025-03-10 23:54:19 +0000

    commit

--- practice/next-greater-element-i-496/main.go
+++ practice/next-greater-element-i-496/main.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
+
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 
+	result := make([]int, 0)
+
+	return result
 }
 
 func main() {
-
+	nums1 := []int{1, 2, 3, 4, 5}
+	nums2 := []int{4, 6, 3, 7}
+	fmt.Println("result : ", nextGreaterElement(nums1, nums2))
 }
