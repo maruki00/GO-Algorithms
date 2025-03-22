ce22796388e589a33e90a5c62c4f7bfcca09c015
maruki00 <aitbaha48@gmail.com>
2025-03-22 12:43:52 +0000

    --

--- practice/substring-with-concatenation-of-all-words-30/main.go
+++ practice/substring-with-concatenation-of-all-words-30/main.go
@@ -3,19 +3,31 @@ package main
 import "fmt"
 
 func findSubstring(s string, words []string) []int {
-	wordsSet := make(map[byte]int)
-	sSet := make(map[byte]int)
+	wordsSet := make(map[string]int)
+
 	result := make([]int, 0)
 	for _, w := range words {
-		for i := range w {
-			wordsSet[w[i]]++
-		}
+		wordsSet[w]++
 	}
 	lenght := len(words) * len(words[0])
-	for i := range s[:lenght] {
-		sSet[s[i]]++
+	lW0 := len(words[0])
+	for i := 0; i < len(s); i++ {
+		sSet := make(map[string]int, len(words))
+		for j := i; j < i+lenght && j < len(s); j += lW0 {
+			sSet[s[i:i+lW0]]++
+		}
+		isOk := true
+		for k, w := range sSet {
+			if exists, ok := wordsSet[k]; !ok || w != exists {
+				isOk = false
+				break
+			}
+		}
+		if isOk {
+			result = append(result, i)
+			i += lenght
+		}
 	}
-	fmt.Println(lenght, sSet, s[:lenght])
 	return result
 }
 
