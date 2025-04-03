a1ee9d39b1e723c98710aea7978b80e53efa1b2c
maruki00 <aitbaha48@gmail.com>
2025-04-03 23:21:57 +0000

    commit

--- practice/reverse-words-in-a-string-151/main.go
+++ practice/reverse-words-in-a-string-151/main.go
@@ -5,11 +5,28 @@ import (
 	"strings"
 )
 
+func split(s string) []string {
+	i := 0
+	result := make([]string, 0)
+	for i < len(s) {
+		for i < len(s) && s[i] == ' ' {
+			i++
+		}
+		j := i
+		for i < len(s) && s[i] != ' ' {
+			i++
+		}
+		if i != j {
+			result = append(result, string(s[j:i]))
+		}
+	}
+	return result
+}
 func reverseWords(s string) string {
-	sts := strings.Split(s, " ")
+	sts := split(s)
 	star, end := 0, len(sts)-1
 	for star <= end {
-		sts[star], sts[end] = strings.TrimSpace(sts[end]), strings.TrimSpace(sts[star])
+		sts[star], sts[end] = sts[end], sts[star]
 		end--
 		star++
 	}
