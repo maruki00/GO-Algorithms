b8317a5219146e724c0c7b0cb84277e0c679116b
maruki00 <aitbaha48@gmail.com>
2025-04-03 18:38:28 +0000

    --

--- practice/merge-strings-alternately-75/main.go
+++ practice/merge-strings-alternately-75/main.go
@@ -1,26 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func mergeAlternately(word1 string, word2 string) string {
 	l := len(word1)
 	r := len(word2)
-	result := ""
+	var result strings.Builder
 	i, j := 0, 0
 	for i < l && j < r {
-		result += string(word1[i]) + string(word2[j])
+		result.WriteByte(word1[i])
+		result.WriteByte(word2[j])
 		i++
 		j++
 	}
 	for i < l {
-		result += string(word1[i])
+		result.WriteByte(word1[i])
 		i++
 	}
 	for j < r {
-		result += string(word2[j])
+		result.WriteByte(word2[j])
 		j++
 	}
-	return result
+	return result.String()
 
 }
 
