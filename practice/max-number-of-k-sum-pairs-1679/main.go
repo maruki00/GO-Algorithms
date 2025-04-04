30e0615be29521b92eff0efa569e65942fd53e6b
maruki00 <aitbaha48@gmail.com>
2025-04-04 23:06:22 +0000

    commit

---
+++ practice/max-number-of-k-sum-pairs-1679/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func maxOperations(nums []int, k int) int {
+
+}
+
+func main() {
+	nums := []int{3, 1, 3, 4, 3}
+	k := 6
+	fmt.Println("result : ", maxOperations(nums, k))
+}
