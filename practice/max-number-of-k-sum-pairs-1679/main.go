240b7641f7daab75790ffc1e7a844bebf92ce6c3
maruki00 <aitbaha48@gmail.com>
2025-04-04 23:23:32 +0000

    commit

--- practice/max-number-of-k-sum-pairs-1679/main.go
+++ practice/max-number-of-k-sum-pairs-1679/main.go
@@ -3,7 +3,30 @@ package main
 import "fmt"
 
 func maxOperations(nums []int, k int) int {
+	pairs := make(map[int]int)
+	for _, i := range nums {
+		pairs[i]++
+	}
+	counter := 0
+	for _, num := range nums {
 
+		nm1, ok := pairs[num]
+		if nm1 <= 0 || !ok {
+			continue
+		}
+		nm2, ok := pairs[k-num]
+		if nm2 <= 0 || !ok {
+			continue
+		}
+
+		if num == k-num && nm1 == 1 {
+			continue
+		}
+		pairs[k-num]--
+		pairs[num]--
+		counter++
+	}
+	return counter
 }
 
 func main() {
