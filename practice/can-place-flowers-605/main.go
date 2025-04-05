4d4decd7b54c905f85bc21f2321162839248c7d0
maruki00 <aitbaha48@gmail.com>
2025-04-05 17:56:24 +0000

    commit

--- practice/can-place-flowers-605/main.go
+++ practice/can-place-flowers-605/main.go
@@ -2,12 +2,39 @@ package main
 
 import "fmt"
 
-func CanPlaceFlowers(flowerbed []int, n int) bool {
-	return false
+func CanPlaceFlowers(nums []int, n int) bool {
+
+	for i := 0; i < len(nums); i++ {
+		if n <= 0 {
+			break
+		}
+		if len(nums) == 1 && nums[0] == 0 {
+			return true
+		}
+		if nums[i] == 1 {
+			continue
+		}
+		if i == 0 && nums[i+1] != 1 {
+			nums[i] = 1
+			n--
+			continue
+		}
+
+		if i == len(nums)-1 && nums[i-1] != 1 {
+			nums[i] = 1
+			n--
+			continue
+		}
+		if i > 0 && nums[i-1] != 1 && nums[i+1] != 1 {
+			nums[i] = 1
+			n--
+		}
+	}
+	return n <= 0
 }
 
 func main() {
-	flowerbed := []int{}
-	n := 1
+	flowerbed := []int{1, 0, 0, 0, 0, 0, 1}
+	n := 2
 	fmt.Println("result : ", CanPlaceFlowers(flowerbed, n))
 }
