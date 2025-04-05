6b7e12b58f9bb595e6d588f3c21c4200718e96b6
maruki00 <aitbaha48@gmail.com>
2025-04-05 11:29:14 +0000

    commit

---
+++ practice/greatest-common-divisor-of-strings-1071/mian.go
@@ -0,0 +1,13 @@
+package main
+
+func gcdOfStrings(str1 string, str2 string) string {
+	for i := 0; i < len(str1)-len(str2); i++ {
+		for j := 0; j < len(str2); j++ {
+
+		}
+	}
+}
+
+func main() {
+
+}
