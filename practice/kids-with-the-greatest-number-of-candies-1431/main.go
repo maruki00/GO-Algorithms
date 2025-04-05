e0eedf737469119687a4c9bdaac4c9bad16ca9c3
maruki00 <aitbaha48@gmail.com>
2025-04-05 15:12:52 +0000

    commit

---
+++ practice/kids-with-the-greatest-number-of-candies-1431/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "math"
+
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+
+	maxCandies := math.MinInt
+
+	for i := range candies {
+		maxCandies = max(maxCandies, candies[i])
+	}
+	result := make([]bool, len(candies))
+	// In the given code snippet, `candie` is a variable that represents the number of candies for
+	// a particular child in the `candies` slice. It is used in the loop to iterate over each
+	// child's candies and check if adding `extraCandies` to the current child's candies would make
+	// them have the maximum number of candies among all children.
+	for i, candie := range candies {
+		result[i] = candie+extraCandies >= maxCandies
+	}
+	return result
+}
+func main() {
+
+}
