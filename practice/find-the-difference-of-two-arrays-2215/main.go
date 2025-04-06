df71c829406a430fd1643fd93726759c97acbb6f
maruki00 <aitbaha48@gmail.com>
2025-04-06 15:10:22 +0100

    commit

--- practice/find-the-difference-of-two-arrays-2215/main.go
+++ practice/find-the-difference-of-two-arrays-2215/main.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 func findDifference(nums1 []int, nums2 []int) [][]int {
-	nums1Map := make(map[int]int)
-	nums2Map := make(map[int]int)
+	nums1Map := make(map[int]bool)
+	nums2Map := make(map[int]bool)
 	result := make([][]int, 2)
 	for _, i := range nums1 {
-		nums1Map[i]++
+		nums1Map[i] = true
 	}
 	for _, i := range nums2 {
-		nums2Map[i]++
+		nums2Map[i] = true
 	}
-	for k, v := range nums2 {
-		if ext, ok := nums1Map[k]; !ok || ext != v {
-			result[0] = append(result[0], v)
+	for k, _ := range nums2Map {
+		if ext, ok := nums1Map[k]; !(ok && ext) {
+			result[0] = append(result[0], k)
 		}
 	}
-	for k, v := range nums1 {
-		if ext, ok := nums2Map[k]; !ok || ext != v {
-			result[1] = append(result[1], v)
+	for k, _ := range nums1Map {
+		if ext, ok := nums2Map[k]; !(ok && ext) {
+			result[1] = append(result[1], k)
 		}
 	}
 	return result
