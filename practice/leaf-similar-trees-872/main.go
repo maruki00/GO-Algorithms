a7d0ec5b73b82a40d901f2e50371416d086add77
maruki00 <aitbaha48@gmail.com>
2025-04-07 23:01:34 +0100

    commit

---
+++ practice/leaf-similar-trees-872/main.go
@@ -0,0 +1,16 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+
+	return false
+}
+
+func main() {
+
+}
