09161032c44a9d7e014100673f85c8ebd27b6683
maruki00 <aitbaha48@gmail.com>
2025-04-08 21:45:56 +0100

    commit

--- practice/longest-subarray-of-1s-after-deleting-one-element-1493/main.go
+++ practice/longest-subarray-of-1s-after-deleting-one-element-1493/main.go
@@ -1,23 +1,33 @@
 package main
 
+import "fmt"
+
 func longestSubarray(nums []int) int {
-	j := 0
-	numOfZeros := 0
+	// to be more effecient we should
+	// optimize the code from O(n^2)
+	// to O(n) using window sliding
 	maxOnes := 0
-	for i:=0;i<len(nums);i++ {
-		for nums[j] != 0 {
-			i++
+	for i := range nums {
+		hasZero := 0
+		j := i
+		for ; j < len(nums); j++ {
+			if nums[j] == 1 {
+				continue
+			}
+			if nums[j] == 0 && hasZero == 1 {
+				break
+			}
+			if nums[j] == 0 {
+				hasZero = 1
+				continue
+			}
 		}
-		j = i
-		for nums[j] == 1 {
-			j++
-		}
-		maxOnes = max(maxOnes, j-i)
-		i = j
+		maxOnes = max(maxOnes, j-i-1)
 	}
 	return maxOnes
 }
 
 func main() {
-
+	nums := []int{0, 1, 1, 1, 0, 1, 1, 0, 1}
+	fmt.Println("result : ", longestSubarray(nums))
 }
