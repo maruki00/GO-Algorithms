5e77aec68805818ba24a4e57ca0a6c3600ea4456
maruki00 <aitbaha48@gmail.com>
2025-04-09 14:30:24 +0100

    commit

--- find-pivot-index-724/main.go
+++ find-pivot-index-724/main.go
@@ -3,23 +3,14 @@ package main
 import "fmt"
 
 func pivotIndex(nums []int) int {
-	result, sumArray := 0, 0
-	j, i := 0, len(nums)-1
+	result := 0
 	sumLift := make([]int, len(nums))
 	sumRight := make([]int, len(nums))
-	for i := range nums {
-		sumArray += nums[i]
+	for i := 0; i < len(nums); i++ {
+		sumLift[i] = nums[i]
+		sumRight[i] = nums[i]
 	}
-	sumRight[i] = sumArray - nums[i]
-	sumLift[j] = sumArray - nums[j]
-	i--
-	j++
-	for ; i >= 0; i-- {
-		sumLift[i] -= nums[i]
-	}
-	for ; j < len(nums); j++ {
-		sumRight[j] -= nums[j]
-	}
+
 	for i := 0; i < len(nums); i++ {
 		if sumLift[i] == sumRight[i] {
 			result++
