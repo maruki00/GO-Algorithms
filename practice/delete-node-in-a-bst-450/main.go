72ec6c799b2e5c267dace30c9f0e0b419108c17b
user <aitbaha48@gmail.com>
2025-04-11 15:18:03 +0100

    commit

--- practice/delete-node-in-a-bst-450/main.go
+++ practice/delete-node-in-a-bst-450/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -9,6 +11,28 @@ type TreeNode struct {
 func deleteNode(root *TreeNode, key int) *TreeNode {
 
 }
-func main() {
 
+func buildTree() *TreeNode {
+	return &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val: 2,
+			},
+			Right: &TreeNode{
+				Val: 4,
+			},
+		},
+		Right: &TreeNode{
+			Val: 6,
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+}
+func main() {
+	root := buildTree()
+	fmt.Println("result : ", root)
 }
