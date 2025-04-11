da023147b132413984623ce764592332f085b9e5
user <aitbaha48@gmail.com>
2025-04-11 16:53:48 +0100

    commit

--- practice/delete-node-in-a-bst-450/main.go
+++ practice/delete-node-in-a-bst-450/main.go
@@ -10,27 +10,35 @@ type TreeNode struct {
 
 func deleteNode(root *TreeNode, key int) *TreeNode {
 	tmp := root
-
+	if tmp.Val == key && tmp.Left == tmp.Right && tmp.Right == nil {
+		return nil
+	}
 	var dfs func(root *TreeNode)
 	dfs = func(root *TreeNode) {
 		if root == nil {
 			return
-		}
-
-		if root.Val == key {
+		} else if root.Val == key {
 			if root.Left == nil && root.Right == nil {
+				root.Val = 0
+				root.Left = nil
+				root.Right = nil
 				root = nil
 				return
 			}
-			if root.Left != nil && root.Left.Val > key {
+			if root.Left != nil && root.Left.Val < key {
 				root.Left.Val, root.Val = root.Val, root.Left.Val
+				if root.Left.Left == nil && root.Left.Right == nil {
+					root.Left = nil
+				}
 				dfs(root.Left)
 			} else {
 				root.Right.Val, root.Val = root.Val, root.Right.Val
+				if root.Right.Left == nil && root.Right.Right == nil {
+					root.Right = nil
+				}
 				dfs(root.Right)
 			}
-		}
-		if root.Val < key {
+		} else if root.Val > key {
 			dfs(root.Left)
 		} else {
 			dfs(root.Right)
@@ -60,7 +68,25 @@ func buildTree() *TreeNode {
 		},
 	}
 }
+
+func print(root *TreeNode) {
+	tmp := root
+	var dfs func(root *TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		fmt.Println("val : ", root.Val)
+		dfs(root.Left)
+		dfs(root.Right)
+	}
+	dfs(tmp)
+}
 func main() {
 	root := buildTree()
-	fmt.Println("result : ", root)
+	result := deleteNode(root, 5)
+	print(root)
+	println("-------------")
+	print(result)
+	//fmt.Println("result : ", root)
 }
