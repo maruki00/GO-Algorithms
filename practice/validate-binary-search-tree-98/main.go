54c852d321a4451eb54d57c7c3e8e78270188cb8
user <aitbaha48@gmail.com>
2025-04-14 16:48:39 +0100

    commit

---
+++ practice/validate-binary-search-tree-98/main.go
@@ -0,0 +1,49 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isValidBST(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	var dfs func(root *TreeNode, val int) bool
+	dfs = func(root *TreeNode, val int) bool {
+		if root == nil {
+			return true
+		}
+		val += root.Val
+		if root.Left != nil && root.Val <= root.Left.Val && val < root.Left.Val {
+			return false
+		}
+		if root.Right != nil && root.Val >= root.Right.Val && val > root.Right.Val {
+			return false
+		}
+		return dfs(root.Left, root.Val+val) && dfs(root.Right, root.Val+val)
+	}
+	return dfs(root, 0)
+}
+
+func main() {
+	// root = [5,4,6,null,null,3,7]
+
+	root := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 4,
+		},
+		Right: &TreeNode{
+			Val: 6,
+			Left: &TreeNode{
+				Val: 3,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+}
