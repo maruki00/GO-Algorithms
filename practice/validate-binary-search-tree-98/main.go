fc66956ddea367609cf0c505edd808ef49fac7ee
user <aitbaha48@gmail.com>
2025-04-14 18:42:07 +0100

    commit

--- practice/validate-binary-search-tree-98/main.go
+++ practice/validate-binary-search-tree-98/main.go
@@ -15,13 +15,13 @@ func isValidBST(root *TreeNode) bool {
 			return true
 		}
 		val += root.Val
-		if root.Left != nil && val < root.Left.Val {
+		if root.Left != nil && val <= root.Left.Val {
 			return false
 		}
-		if root.Right != nil && val > root.Right.Val {
+		if root.Right != nil && val >= root.Right.Val {
 			return false
 		}
-		return dfs(root.Left, root.Val+val) && dfs(root.Right, root.Val+val)
+		return dfs(root.Left, root.Val) && dfs(root.Right, root.Val+val)
 	}
 	return dfs(root, 0)
 }
@@ -29,20 +29,21 @@ func isValidBST(root *TreeNode) bool {
 func main() {
 	// root = [5,4,6,null,null,3,7]
 
-	root := &TreeNode{
-		Val: 5,
-		Left: &TreeNode{
-			Val: 4,
-		},
-		Right: &TreeNode{
-			Val: 6,
-			Left: &TreeNode{
-				Val: 3,
-			},
-			Right: &TreeNode{
-				Val: 7,
-			},
-		},
-	}
+	// root := &TreeNode{
+	// 	Val: 5,
+	// 	Left: &TreeNode{
+	// 		Val: 4,
+	// 	},
+	// 	Right: &TreeNode{
+	// 		Val: 6,
+	// 		Left: &TreeNode{
+	// 			Val: 3,
+	// 		},
+	// 		Right: &TreeNode{
+	// 			Val: 7,
+	// 		},
+	// 	},
+	// }
+	root := &TreeNode{Val: 2, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 2}}
 	fmt.Println("result : ", isValidBST(root))
 }
