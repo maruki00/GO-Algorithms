a7180825b655e898180227d82ee9afcadd8d6ef3
maruki00 <aitbaha48@gmail.com>
2025-04-22 23:57:38 +0100

    commit

---
+++ practice/lowest-common-ancestor-of-a-binary-search-tree-235/main.go
@@ -0,0 +1,24 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else if p.Val > root.Val && q.Val > root.Val {
+			root = root.Right
+		} else {
+			return root
+		}
+	}
+	return nil
+}
+
+func main() {
+
+}
