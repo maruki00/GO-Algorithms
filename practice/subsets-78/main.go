e24e389f626040e8a4cddacc05fc47f0de42fa3f
maruki00 <aitbaha48@gmail.com>
2025-04-24 22:40:16 +0100

    commit

--- practice/subsets-78/main.go
+++ practice/subsets-78/main.go
@@ -1,12 +1,31 @@
 package main
 
+import "fmt"
+
 func subsets(nums []int) [][]int {
 
 	result := make([][]int, 0)
+
+	var backtracking func(nms []int, indx int)
+	backtracking = func(nms []int, indx int) {
+		if len(nms) == 0 || indx >= len(nums) {
+			return
+		}
+
+		result = append(result, nms)
+		backtracking(append(nms, nums[indx]), indx+1)
+		// if len(nms) <= 0 {
+		// 	return
+		// }
+		// nms = nms[:len(nms)-1]
+	}
+
+	backtracking(nums, 0)
 
 	return result
 }
 
 func main() {
-
+	nums := []int{1, 2, 3}
+	fmt.Println("result : ", subsets(nums))
 }
