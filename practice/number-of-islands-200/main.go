3691c6be20cfcfbf84c9643bcb402dd3302fa00a
maruki00 <aitbaha48@gmail.com>
2025-04-27 18:59:14 +0100

    commit

--- practice/number-of-islands-200/main.go
+++ practice/number-of-islands-200/main.go
@@ -1,9 +1,42 @@
 package main
 
-func numIslands(grid [][]byte) int {
+import "fmt"
 
+func numIslands(grid [][]byte) int {
+	count := 0
+
+	var dfs func(grid [][]byte, i, j int)
+	dfs = func(grid [][]byte, i, j int) {
+		if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[i]) || grid[i][j] != '1' {
+			return
+		}
+
+		grid[i][j] = '0'
+
+		dfs(grid, i+1, j)
+		dfs(grid, i-1, j)
+		dfs(grid, i, j+1)
+		dfs(grid, i, j-1)
+	}
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				dfs(grid, i, j)
+				count++
+			}
+		}
+	}
+	return count
 }
 
 func main() {
+	grid := [][]byte{
+		{'1', '1', '1', '1', '0'},
+		{'1', '1', '0', '1', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '0', '0', '0'},
+	}
 
+	fmt.Println("result : ", numIslands(grid))
 }
