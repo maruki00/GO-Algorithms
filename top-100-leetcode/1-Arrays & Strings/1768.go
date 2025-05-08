2b551d619558b64f5a163b63a2454a3d04103e69
maruki00 <aitbaha48@gmail.com>
2025-05-08 17:26:49 +0100

    commit

--- top-100-leetcode/1-Arrays & Strings/1768.go
+++ top-100-leetcode/1-Arrays & Strings/1768.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
 You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 Return the merged string.
@@ -33,22 +35,24 @@ func mergeAlternately(word1 string, word2 string) string {
 	mergedWords := make([]byte, len(word1)+len(word2))
 	i, j := 0, 0
 	for i < len(word1) && j < len(word2) {
-		mergedWords[i+j+1] = word1[i]
-		mergedWords[i+j+2] = word2[j]
+		mergedWords[i+j] = word1[i]
+		mergedWords[i+j+1] = word2[j]
 		i++
 		j++
 	}
 	for i < len(word1) {
-		mergedWords[i+j+1] = word1[i]
+		mergedWords[i+j] = word1[i]
 		i++
 	}
 	for j < len(word2) {
-		mergedWords[i+j+1] = word2[j]
+		mergedWords[i+j] = word2[j]
 		j++
 	}
 	return string(mergedWords)
 }
 
-func mian() {
-
+func main() {
+	word1 := "abc"
+	word2 := "defjh"
+	fmt.Println("result : ", mergeAlternately(word1, word2))
 }
