fde27816fd3471606f451785a167d6980a71f22e
maruki00 <aitbaha48@gmail.com>
2025-05-08 16:46:55 +0100

    commit

---
+++ top-100-leetcode/1-Arrays & Strings/2239.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/**
+Given an integer array nums of size n, return the number with the value closest to 0 in nums. If there are multiple answers, return the number with the largest value.
+
+Example 1:
+Input: nums = [-4,-2,1,4,8]
+Output: 1
+Explanation:
+The distance from -4 to 0 is |-4| = 4.
+The distance from -2 to 0 is |-2| = 2.
+The distance from 1 to 0 is |1| = 1.
+The distance from 4 to 0 is |4| = 4.
+The distance from 8 to 0 is |8| = 8.
+Thus, the closest number to 0 in the array is 1.
+
+Example 2:
+Input: nums = [2,-1,1]
+Output: 1
+Explanation: 1 and -1 are both the closest numbers to 0, so 1 being larger is returned.
+
+*/
+
+func findClosestNumber(nums []int) int {
+	var closest = nums[0]
+	for i := range nums {
+		fmt.Println("items : ", int(math.Abs(float64(0-nums[i]))), i, nums[i])
+		closest = min(closest, int(math.Abs(float64(0-nums[i]))))
+	}
+	return closest
+}
+
+func main() {
+	nums := []int{-4, -2, 1, 4, 8}
+	fmt.Println("result : ", findClosestNumber(nums))
+}
